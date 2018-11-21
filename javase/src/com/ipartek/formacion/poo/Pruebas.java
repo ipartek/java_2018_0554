cc2440aee0007ec8f6768cb42d457a9adaa40258
JavierLete <JavierLete@10.0.2.15>
2018-11-21 11:19:12 +0100

    Cambio Pruebas iniciales de Usuario

--- javase/src/com/ipartek/formacion/poo/Pruebas.java
+++ javase/src/com/ipartek/formacion/poo/Pruebas.java
@@ -7,19 +7,33 @@ public class Pruebas {
 		
 		//usuario = new Usuario();
 		
-		Usuario usuario = new Usuario();
+		System.out.println(Usuario.getContadorDeInstancias());
 		
-//		usuario.setEmail("javierleteñldskfj.asdf");
-//		usuario.setPassword("contra");
+		Usuario usuario1 = new Usuario("javierlete@email.net", "contraseña");
 		
-		System.out.println(usuario.getEmail());
-		System.out.println(usuario.getPassword());
+		System.out.println(Usuario.getContadorDeInstancias());
 		
-		usuario.cambiarPassword("contra");
+		System.out.println(new Usuario().getEmail());
 		
-		System.out.println(usuario.datosCompletos());
-		System.out.println(usuario.datosCompletos("coma"));
-		System.out.println(usuario.datosCompletos("corchetes"));
+		System.out.println(Usuario.getContadorDeInstancias());
+		
+		Usuario usuario2 = new Usuario();
+		
+		System.out.println(Usuario.getContadorDeInstancias());
+		
+		usuario2.setEmail("javierleteñl@dskfj.asdf");
+		usuario2.setPassword("contra");
+
+		System.out.println(usuario2);
+		
+		System.out.println(usuario1.getEmail());
+		System.out.println(usuario1.getPassword());
+		
+		usuario1.cambiarPassword("contra");
+		
+		System.out.println(usuario1.datosCompletos());
+		System.out.println(usuario1.datosCompletos("coma"));
+		System.out.println(usuario1.datosCompletos("corchetes"));
 	}
 
 }
