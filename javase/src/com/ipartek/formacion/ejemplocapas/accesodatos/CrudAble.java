f59adcaa5215f8537909876ead2878250b6b645d
JavierLete <JavierLete@10.0.2.15>
2018-11-27 10:07:06 +0100

    CrudAble con default (Java8)

--- javase/src/com/ipartek/formacion/ejemplocapas/accesodatos/CrudAble.java
+++ javase/src/com/ipartek/formacion/ejemplocapas/accesodatos/CrudAble.java
@@ -2,10 +2,20 @@ package com.ipartek.formacion.ejemplocapas.accesodatos;
 
 import java.util.List;
 
-public interface CrudAble<Pojo> {
-	public List<Pojo> getAll();
-	public Pojo getById(Long id);
-	public void insert(Pojo pojo);
-	public void update(Pojo pojo);
-	public void delete(Pojo pojo);
+public interface CrudAble<Pojo> { // default existe a partir de Java 8
+	public default List<Pojo> getAll() {
+		throw new RuntimeException("No implementado");
+	}
+	public default Pojo getById(Long id) {
+		throw new RuntimeException("No implementado");
+	}
+	public default void insert(Pojo pojo) {
+		throw new RuntimeException("No implementado");
+	}
+	public default void update(Pojo pojo) {
+		throw new RuntimeException("No implementado");
+	}
+	public default void delete(Long id) {
+		throw new RuntimeException("No implementado");
+	}
 }
