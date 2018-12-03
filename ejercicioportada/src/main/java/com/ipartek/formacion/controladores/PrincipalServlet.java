1af73b48c5346d744254e3ddfa69bfb90c2361c7
JavierLete <JavierLete@10.0.2.15>
2018-12-03 12:22:26 +0100

    Mover PrincipalServlet a controladores

--- ejercicioportada/src/main/java/com/ipartek/formacion/controladores/PrincipalServlet.java
+++ ejercicioportada/src/main/java/com/ipartek/formacion/controladores/PrincipalServlet.java
@@ -1,15 +1,26 @@
-package com.ipartek.formacion;
+package com.ipartek.formacion.controladores;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ipartek.formacion.pojos.Proyecto;
+
 public class PrincipalServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArrayList<Proyecto> proyectos = new ArrayList<Proyecto>();
+		
+		proyectos.add(new Proyecto(1L, "Primer proyecto", "Este proyecto mola mogollón porque bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla"));
+		proyectos.add(new Proyecto(2L, "Segundo proyecto", "Este proyecto es una birria porque bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla"));
+		
+		request.setAttribute("proyectos", proyectos);
+		
 		request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 
