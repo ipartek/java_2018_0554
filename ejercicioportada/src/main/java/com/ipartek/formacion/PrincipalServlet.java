03d632a30d43235c698e9969e771bcc7ce5b77ee
JavierLete <JavierLete@10.0.2.15>
2018-12-03 12:14:09 +0100

    Cambio a JSP y mapeado desde servlet, con inicio por defecto

---
+++ ejercicioportada/src/main/java/com/ipartek/formacion/PrincipalServlet.java
@@ -0,0 +1,20 @@
+package com.ipartek.formacion;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PrincipalServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
