4f039eb0fbcb4f5bc2e136cfe84727b1df8c8115
JavierLete <JavierLete@10.0.2.15>
2018-12-03 13:10:53 +0100

    Mapeado a través de anotaciones

--- ejercicioportada/src/main/java/com/ipartek/formacion/controladores/PrincipalServlet.java
+++ ejercicioportada/src/main/java/com/ipartek/formacion/controladores/PrincipalServlet.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.ipartek.formacion.pojos.Proyecto;
 
+@WebServlet("/principal")
 public class PrincipalServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
