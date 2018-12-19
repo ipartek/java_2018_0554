3af78a353b13d2d6860839c89126e0580ee1f505
imaher <imahernando@gmail.com>
2018-12-19 10:49:35 +0100

    Usuarios con excepciones y verificados

--- javaee/src/main/java/com/ipartek/formacion/ejemplocapas/controladores/UsuarioController.java
+++ javaee/src/main/java/com/ipartek/formacion/ejemplocapas/controladores/UsuarioController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.ipartek.formacion.modelo.daos.UsuarioDAO;
+import com.ipartek.formacion.modelo.pojos.PojoException;
 import com.ipartek.formacion.modelo.pojos.Usuario;
 
 /**
@@ -42,21 +43,19 @@ public class UsuarioController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		try {
-			//recoger parametros del formulario
-			
-			
+		//SE RECOGEN LOS PARAMETEROS COMO STRING SIEMPRE LUEGO YA SE MANIPULAN
 			String id = request.getParameter("id");
 			String email = request.getParameter("email");
 			String pass = request.getParameter("pass");
+		
+		try {
+			//recoger parametros del formulario
 			Usuario uNuevo = new Usuario();	
 		
 			
 			
 			try {
-				try {
+				
 				//crear usuario nuevo   TODO hacer insert en base datos
 				uNuevo = new Usuario();		
 				uNuevo.setId(Long.parseLong(id));
@@ -66,12 +65,16 @@ public class UsuarioController extends HttpServlet {
 			}catch (NumberFormatException e) {
 				uNuevo = null;
 				request.setAttribute("mensaje", "El id es incorrecto");
-			}	
+				
+				
+			}catch (PojoException e) {
+				uNuevo = null;
+				request.setAttribute("mensaje", e.getMessage());
+				
+					
 			}catch (Exception e) {
 				uNuevo = null;
-				request.setAttribute("id", id);
-				request.setAttribute("email", email);
-				request.setAttribute("pass", pass);
+				
 				request.setAttribute("mensaje", "Los datos del usuario no son validos");
 			}
 			
@@ -95,6 +98,9 @@ public class UsuarioController extends HttpServlet {
 		}finally {
 			
 			// Ir a JSP o VISTA
+			request.setAttribute("id", id);
+			request.setAttribute("email", email);
+			request.setAttribute("pass", pass);
 			request.getRequestDispatcher("usuarios.jsp").forward(request, response);
 		}	
 			
