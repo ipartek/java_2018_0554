71ee9178434fb430f95bcdd66175deafdede0684
anderuraga <ander.uraga@gmail.com>
2018-12-20 10:16:31 +0100

    nuevo projecto 3.0

---
+++ javaee3/src/main/java/com/ipartek/formacion/controller/LoginController.java
@@ -0,0 +1,46 @@
+package com.ipartek.formacion.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ipartek.formacion.modelo.pojo.Video;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/login")
+public class LoginController extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+       
+    
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String usuario = request.getParameter("usuario");
+		String pass = request.getParameter("pass");
+		
+		
+		request.setAttribute("video", new Video());
+		request.setAttribute("mensaje", "usuario erroneo " + usuario );
+		
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
