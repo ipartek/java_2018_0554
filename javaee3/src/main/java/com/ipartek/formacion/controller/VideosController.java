6e22ec7b38b660835ad00ec50c83763f1149403e
AnderSolana <andersolanad@gmail.com>
2018-12-21 10:14:57 +0100

    Cambios varios. Includes para las páginas

--- javaee3/src/main/java/com/ipartek/formacion/controller/VideosController.java
+++ javaee3/src/main/java/com/ipartek/formacion/controller/VideosController.java
@@ -9,7 +9,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.ipartek.formacion.modelo.dao.VideoDAO;
 import com.ipartek.formacion.modelo.pojo.Video;
 
