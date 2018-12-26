3d13122b3a3c848c046f5f6158ced320276fc8a4
Curso <Curso@Formacion-TOSH1.ipartekaula.com>
2018-12-26 09:04:31 +0100

    controlado insercion por get pagina mayor al tamaño del libro

--- librocompartido/src/main/java/com/ipartek/formacion/controller/PaginaSrvLt.java
+++ librocompartido/src/main/java/com/ipartek/formacion/controller/PaginaSrvLt.java
@@ -16,8 +16,6 @@ public class PaginaSrvLt extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	ArrayList<Pagina> libro;
 
-
-
 	@Override
 	public void init() throws ServletException {
 		super.init();
@@ -25,8 +23,7 @@ public class PaginaSrvLt extends HttpServlet {
 		libro.add(new Pagina("autor1", "texto1"));
 		libro.add(new Pagina("autor2", "texto2"));
 		libro.add(new Pagina("autor3", "texto3"));
-		
-		
+
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -38,22 +35,35 @@ public class PaginaSrvLt extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
 		int paginaactual = 0;
-		
-		String error= (String) request.getAttribute("error");
-		ArrayList<Pagina> libro=(ArrayList<Pagina>) request.getServletContext().getAttribute("libro");
-		String recibepagina = request.getParameter("pag");
-		if (recibepagina != null) {
-			paginaactual = Integer.parseInt(recibepagina);
+		String error = null;
+		ArrayList<Pagina> libro = null;
+
+		try {
+			paginaactual = 0;
+
+			error = (String) request.getAttribute("error");
+			libro = (ArrayList<Pagina>) request.getServletContext().getAttribute("libro");
+			String recibepagina = request.getParameter("pag");
+			if (recibepagina != null) {
+				paginaactual = Integer.parseInt(recibepagina);
+				if (paginaactual > libro.size() - 1) {
+					error="Pagina no valida";
+					paginaactual = libro.size() - 1;
+				}
+			}
+		} catch (NumberFormatException e) {
+			error="Pagina NO disponible";
+			request.getRequestDispatcher("pagina?pag=0").forward(request, response);
+			e.printStackTrace();
+		} finally {
+			request.setAttribute("paginaactual", paginaactual);
+			request.setAttribute("totalpaginas", libro.size());
+			request.setAttribute("pagina", libro.get(paginaactual));
+			request.setAttribute("error", error);
+
+			request.getRequestDispatcher("libro.jsp").forward(request, response);
 		}
-
-		request.setAttribute("paginaactual", paginaactual);
-		request.setAttribute("totalpaginas", libro.size());
-		request.setAttribute("pagina", libro.get(paginaactual));
-		request.setAttribute("error", error);
-
-		request.getRequestDispatcher("libro.jsp").forward(request, response);
 
 	}
 
