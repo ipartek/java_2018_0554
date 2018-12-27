a26ff6b8753285a972561db36497a350b1df1de1
anderuraga <ander.uraga@gmail.com>
2018-12-27 09:36:25 +0100

    properties para base datos y Log4j

--- javaee3/src/main/java/com/ipartek/formacion/modelo/ConnectionManager.java
+++ javaee3/src/main/java/com/ipartek/formacion/modelo/ConnectionManager.java
@@ -1,30 +1,48 @@
 package com.ipartek.formacion.modelo;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
 
 public class ConnectionManager {
 
+	private final static Logger LOG = Logger.getLogger(ConnectionManager.class);
 	private static Connection conn;
-	private static final String URL = "jdbc:mysql://192.168.0.44/youtube?useSSL=false";
-	private static final String USER = "viernes";
-	private static final String PASS = "juernes";
+	
 
 	public static Connection getConnection() {
 
 		conn = null;
-
 		try {
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			conn = DriverManager.getConnection( URL , USER, PASS);
+			//cargar properties
+			Properties prop = new Properties();
+			
+			InputStream input = ConnectionManager.class.getClassLoader().getResourceAsStream("database.properties");	
+			prop.load(input);
+			LOG.debug("cargado fichero properties");
+			
+			//comprobar que exista .jar para mysql
+			Class.forName(prop.getProperty("ddbb.driver")).newInstance();
+			LOG.debug("Existe driver mysql");
+	
+			//crear conexion
+			conn = DriverManager.getConnection(
+								prop.getProperty("ddbb.url"), 
+								prop.getProperty("ddbb.user"),
+								prop.getProperty("ddbb.pass"));
+			
+			LOG.debug("conexion establecida");
 
-		} catch (Exception e) {
-
-			e.printStackTrace();
-		}
-
+		}catch (Exception e) {
+			
+			LOG.fatal("Erro estableciendo conexion base datos" , e);
+			
+		}	
 		return conn;
 
 	}
-	
 }
