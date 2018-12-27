a6c1abc5daef4b29c2ed2796ebb931de33dccbb2
dZallo <danielzallo@gmail.com>
2018-12-27 10:10:19 +0100

    Corrección del listener de usuarios conectados

--- libroCompartido/src/main/java/com/ipartek/formacion/libroCompartido/listeners/UsuarioListener.java
+++ libroCompartido/src/main/java/com/ipartek/formacion/libroCompartido/listeners/UsuarioListener.java
@@ -1,11 +1,9 @@
 package com.ipartek.formacion.libroCompartido.listeners;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.servlet.ServletContext;
 import javax.servlet.annotation.WebListener;
-import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionAttributeListener;
 import javax.servlet.http.HttpSessionBindingEvent;
 
@@ -17,24 +15,22 @@ import com.ipartek.formacion.libroCompartido.modelo.pojo.Usuario;
  */
 @WebListener
 public class UsuarioListener implements HttpSessionAttributeListener {
-	//public static HashMap<String, Usuario> usuariosLogeados = new HashMap<String,Usuario>();
-	public static ArrayList<Usuario> usuariosLogeados = new ArrayList<Usuario>();
+	public static HashMap<String, Usuario> usuariosLogueados = new HashMap<String, Usuario>();
 	
 	/**
      * @see HttpSessionAttributeListener#attributeAdded(HttpSessionBindingEvent)
      */
     public void attributeAdded(HttpSessionBindingEvent event)  { 
-       System.out.println("attributeAdded");
+      
+       System.out.println("Atributo añadido");
        
-       //Obtengo el usuario de la session
-       Usuario u= (Usuario) event.getSession().getAttribute("usuarioLogeado");
-       
-       //Añado el usuario al array de los usuarioLogeados
-       usuariosLogeados.add(u);
-       
+       if (event.getName().equals("usuarioLogeado")) {
+      	 Usuario usuario = (Usuario) event.getValue();
+			usuariosLogueados.put(usuario.getUsuario(), usuario);
+		}
        
        ServletContext appContext = event.getSession().getServletContext();
-       appContext.setAttribute("usuariosLogeados", usuariosLogeados);
+       appContext.setAttribute("usuarios_logueados", usuariosLogueados);
     }
 
 	/**
@@ -42,14 +38,13 @@ public class UsuarioListener implements HttpSessionAttributeListener {
      */
     public void attributeRemoved(HttpSessionBindingEvent event)  { 
     	 System.out.println("attributeRemoved");
-    	
-		if(event.getName().equals("usuarioLogeado")) {
-			usuariosLogeados.remove((Usuario) event.getValue());
+         if (event.getName().equals("usuarioLogeado")) {
+        	 Usuario usuario = (Usuario) event.getValue();
+			usuariosLogueados.remove(usuario.getUsuario());
 		}
          
-	  ServletContext appContext = event.getSession().getServletContext();
-      appContext.setAttribute("usuariosLogeados", usuariosLogeados);
-        
+         ServletContext appContext = event.getSession().getServletContext();
+         appContext.setAttribute("usuarios_logueados", usuariosLogueados);
     }
 
 	/**
--- libroCompartido/src/main/webapp/usuariosLogeados.jsp
+++ libroCompartido/src/main/webapp/usuariosLogeados.jsp
@@ -1,12 +1,26 @@
 <%@include file="/includes/navbar.jsp"%>
 <main role="main" class="container">
 <div class="jumbotron">
-	<h1><fmt:message key="usuariosLogeados.titulo"/></h1>
-	<ul>
-		<c:forEach items="${applicationScope.usuariosLogeados}" var="u">
-			<li>${ u.usuario}</li>
-		</c:forEach>
-	</ul>
+	<h1>
+		<fmt:message key="usuariosLogeados.titulo" />
+	</h1>
+	
+		<table class="table">
+			<thead class="thead-dark">
+				<tr>
+					<th>Nombre</th>
+				</tr>
+			</thead>
+			<tbody class="bg-success">
+				<c:forEach items="${applicationScope.usuarios_logueados}"
+					var="entry">
+					<tr>
+						<td><c:out value="${entry.key}" /></td>
+					</tr>
+				</c:forEach>
+			</tbody>
+		</table>
+	
 </div>
 </main>
 <%@include file="/includes/footer.jsp"%>
