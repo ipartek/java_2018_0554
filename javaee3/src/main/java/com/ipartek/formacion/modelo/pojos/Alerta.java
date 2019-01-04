c069b3650641ef153b15d3a856cb02ffc088cdb6
andonishiraiwa <Curso@Formacion-TOS13.ipartekaula.com>
2019-01-04 12:28:20 +0100

    pojos alerta + Pagina

---
+++ javaee3/src/main/java/com/ipartek/formacion/modelo/pojos/Alerta.java
@@ -0,0 +1,46 @@
+package com.ipartek.formacion.modelo.pojos;
+
+public class Alerta {
+
+	public static final String TIPO_PRIMARY = "primary";
+	public static final String TIPO_SUCCESS = "success";
+	public static final String TIPO_DANGER = "danger";
+	public static final String TIPO_WARNING = "warning";
+
+	private String texto;
+	private String tipo;
+
+	public Alerta() {
+		super();
+		this.texto = "Error Inexperado";
+		this.tipo = TIPO_DANGER;
+	}
+
+	public Alerta(String tipo, String texto) {
+		this();
+		this.texto = texto;
+		this.tipo = tipo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	@Override
+	public String toString() {
+		return "Alerta [texto=" + texto + ", tipo=" + tipo + "]";
+	}
+
+}
---
+++ javaee3/src/main/java/com/ipartek/formacion/modelo/pojos/Pagina.java
@@ -0,0 +1,45 @@
+package com.ipartek.formacion.modelo.pojos;
+
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class Pagina {
+	
+	@NotEmpty
+	@Size(min=50, max=255)
+	private String texto;
+	
+	@NotEmpty
+	@Size(min=2, max=50)
+	private String autor;
+	
+	public Pagina() {
+		super();
+		this.autor = "";
+		this.texto = "";
+	}
+	
+	public Pagina(String texto, String autor) {
+		this();
+		this.autor = autor;
+		this.texto = texto;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	
+}
