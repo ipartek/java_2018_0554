dfd21eefb4aad127eff7d5460c60247b03aec459
mikel <piresin14@gmail.com>
2019-01-07 12:01:30 +0100

    archivos workspace varios (modelo, sql)

--- libros/bbdd.sql
+++ libros/bbdd.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------------------------
--- Host:                         192.168.0.44
--- Versión del servidor:         5.7.14 - MySQL Community Server (GPL)
--- SO del servidor:              Win64
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.1.37-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win32
 -- HeidiSQL Versión:             9.5.0.5196
 -- --------------------------------------------------------
 
@@ -12,25 +12,63 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- Volcando estructura de base de datos para youtube
-CREATE DATABASE IF NOT EXISTS `youtube` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `youtube`;
+-- Volcando estructura de base de datos para javaee
+CREATE DATABASE IF NOT EXISTS `javaee` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `javaee`;
 
--- Volcando estructura para tabla youtube.mikel_videos
-CREATE TABLE IF NOT EXISTS `mikel_videos` (
+-- Volcando estructura para tabla javaee.categoria
+CREATE TABLE IF NOT EXISTS `categoria` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) DEFAULT NULL,
-  `url` varchar(100) DEFAULT NULL,
+  `nombre` varchar(55) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla youtube.mikel_videos: 3 rows
-/*!40000 ALTER TABLE `mikel_videos` DISABLE KEYS */;
-INSERT INTO `mikel_videos` (`id`, `nombre`, `url`) VALUES
-	(1, 'Fredrik\'s DRIFT', 'TLoAdsj5jRo'),
-	(2, 'Fredrik Sørlie\'s Scandinavian Flickfest', '_1ZauQdvlqo'),
-	(3, 'BRING BACK THE TOUGE', 'Rq2ktZSyoEo');
-/*!40000 ALTER TABLE `mikel_videos` ENABLE KEYS */;
+-- Volcando datos para la tabla javaee.categoria: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+
+-- Volcando estructura para tabla javaee.usuario
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla javaee.usuario: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` (`id`, `email`, `password`) VALUES
+	(1, 'admin@admin.com', '12345678'),
+	(2, 'john@doe.com', 'Pa$$w0rd'),
+	(6, 'lalala@lalala', 'aaaaaaaaaaaa'),
+	(11, 'scooby@wsw', 'wwwwwwwwww'),
+	(12, 'pepe@pepe.com', '11111111111112');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+
+-- Volcando estructura para tabla javaee.video
+CREATE TABLE IF NOT EXISTS `video` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `codigo` varchar(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_categoria` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `codigo` (`codigo`),
+  KEY `FK_ID_USUARIO` (`id_usuario`),
+  KEY `FK_ID_CATEGORIA` (`id_categoria`),
+  CONSTRAINT `FK_ID_CATEGORIA` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id`),
+  CONSTRAINT `FK_ID_USUARIO` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla javaee.video: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `video` DISABLE KEYS */;
+INSERT INTO `video` (`id`, `nombre`, `codigo`, `id_usuario`, `id_categoria`) VALUES
+	(2, 'Fredrik\'s DRIFT mod', 'TLoAdsj5jRo', 1, NULL),
+	(3, 'Fredrik Sørlie\'s Scandinavian Flickfest', '_1ZauQdvlqo', 1, NULL),
+	(8, 'BRINNG BACK THE TOUGE', 'Rq2ktZSyoEo', 1, NULL),
+	(19, 'manolo', '12345678911', 12, NULL);
+/*!40000 ALTER TABLE `video` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
--- libros/crud.sql
+++
@@ -1,4 +0,0 @@
--- insert into video values(null,'lalala','12345678911',1);
--- UPDATE `video` SET nombre='lalala' , codigo='00000000000', id_usuario=2 WHERE id=9;
--- DELETE from video where id=9;
-Select * from video;
