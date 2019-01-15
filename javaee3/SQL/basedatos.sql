537dbcb1ab7a16b3fd956c83920e19cdbcc3482c
anderuraga <ander.uraga@gmail.com>
2019-01-15 10:23:09 +0100

    cambio varias funciones DAO por procedimientos almacenados, actualizado script base datos

--- javaee3/SQL/basedatos.sql
+++ javaee3/SQL/basedatos.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `javaee` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `javaee`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: javaee
@@ -31,7 +29,7 @@ CREATE TABLE `usuario` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +38,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'admin@admin.com','12345678'),(6,'marianiko@pepemail.com','12345678'),(12,'mariano@matriano.com','12345678'),(15,'auraga@ipartek','12345678'),(17,'mariano@matriano.com.es','123456879');
+INSERT INTO `usuario` VALUES (1,'admin@admin.com','12345678'),(6,'marianiko@pepemail.com','12345678'),(12,'mariano@matriano.com','12345678'),(15,'auraga@ipartek','12345678'),(17,'mariano@matriano.com.es','123456879'),(18,'pa','8888888'),(20,'hayavamos@gamil.com','12345678'),(21,'paramertro3@estasi.com','12345678');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -73,6 +71,121 @@ LOCK TABLES `video` WRITE;
 INSERT INTO `video` VALUES (1,'Surf Search Spot 2 0 video promo','LPDhuthFD98',1,'2019-01-14 08:48:01'),(2,'Sojahfjsrduj','94HwzFGH5gY',6,'2019-01-14 08:48:01'),(4,'Fito & Fitipaldis - Los huesos de los besos (Videoclip oficial)','nmoXDU4BAgI',15,'2019-01-14 08:48:01'),(5,'La Fuga - Por verte sonreir','hdQ99wLYc2g',6,'2019-01-14 08:48:01');
 /*!40000 ALTER TABLE `video` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'javaee'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `usuario_contar` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `usuario_contar`( OUT total INT )
+BEGIN
+	
+    -- usamos INTO para asignar valora la variable de salida
+    SELECT count(*) INTO total FROM usuario; 
+    
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `usuario_contar_email` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `usuario_contar_email`( IN cadena VARCHAR(255) , OUT total INT )
+BEGIN
+
+	SELECT COUNT(*) INTO total FROM usuario WHERE email LIKE CONCAT('%',cadena,'%');
+	
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `usuario_getAll` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `usuario_getAll`()
+BEGIN
+ SELECT id, email, password FROM usuario ORDER BY id DESC LIMIT 500;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `usuario_getById` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `usuario_getById`( IN p_id LONG )
+BEGIN
+	
+    SELECT id, email, password FROM usuario WHERE id = p_id;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `usuario_insert` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `usuario_insert`( 
+	IN p_email VARCHAR(150) , 
+    IN p_password VARCHAR(45), 
+    OUT o_id LONG)
+BEGIN
+
+	-- ejecutamos insert
+	INSERT INTO `usuario` (`email`, `password`) VALUES ( p_email, p_password );
+    
+	-- retornamos ID generado
+	SET o_id = LAST_INSERT_ID();
+    
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -83,4 +196,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-14  9:23:57
+-- Dump completed on 2019-01-15 10:21:13
--- javaee3/src/main/java/com/ipartek/formacion/modelo/dao/UsuarioDAO.java
+++ javaee3/src/main/java/com/ipartek/formacion/modelo/dao/UsuarioDAO.java
@@ -1,9 +1,11 @@
 package com.ipartek.formacion.modelo.dao;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.ArrayList;
 
 import com.ipartek.formacion.modelo.ConnectionManager;
@@ -58,13 +60,17 @@ public class UsuarioDAO {
 	public Usuario getById(long id) {
 
 		Usuario usuario = null;
-		String sql = "SELECT id, email, password FROM usuario WHERE id = ?;";
+		String sql = "{call usuario_getById(?)}";
 
-		try (Connection conn = ConnectionManager.getConnection(); PreparedStatement pst = conn.prepareStatement(sql);) {
-			pst.setLong(1, id);
+		try (Connection conn = ConnectionManager.getConnection(); 
+				CallableStatement cs = conn.prepareCall(sql);) {
 			
-			try (ResultSet rs = pst.executeQuery()) {
-				while (rs.next()) { // hemos encontrado usuario
+			// parametro entrada
+			cs.setLong(1, id);
+			
+			try (ResultSet rs = cs.executeQuery()) {
+				while (rs.next()) { 
+					// hemos encontrado usuario
 					usuario = new Usuario();
 					usuario.setId(rs.getLong("id"));
 					usuario.setEmail(rs.getString("email"));
@@ -80,11 +86,11 @@ public class UsuarioDAO {
 	public ArrayList<Usuario> getAll() {
 
 		ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
-		String sql = "SELECT id, email, password FROM usuario ORDER BY id DESC LIMIT 500";
+		String sql = "{call usuario_getAll()}";
 
 		try (Connection conn = ConnectionManager.getConnection();
-				PreparedStatement pst = conn.prepareStatement(sql);
-				ResultSet rs = pst.executeQuery()) {
+				CallableStatement cs = conn.prepareCall(sql);
+				ResultSet rs = cs.executeQuery()) {
 
 			while (rs.next()) {
 				try {
@@ -92,7 +98,7 @@ public class UsuarioDAO {
 					usuario.setId(rs.getLong("id"));
 					usuario.setEmail(rs.getString("email"));
 					usuario.setPassword(rs.getString("password"));
-					// a�adir en array
+					// añadir en array
 					usuarios.add(usuario);
 				} catch (Exception e) {
 					System.out.println("usuario no valido");
@@ -109,13 +115,22 @@ public class UsuarioDAO {
 	public boolean insert(Usuario u) throws SQLException {
 
 		boolean resul = false;
-		String sql = "INSERT INTO `usuario` (`email`, `password`) VALUES (?,?);";
-		try (Connection conn = ConnectionManager.getConnection(); PreparedStatement pst = conn.prepareStatement(sql);) {
+		String sql = "{call usuario_insert(?,?,?)}";
+		try (
+				Connection conn = ConnectionManager.getConnection(); 
+				CallableStatement cs = conn.prepareCall(sql);) {
 
-			pst.setString(1, u.getEmail());
-			pst.setString(2, u.getPassword());
-			int affectedRows = pst.executeUpdate();
+			//parametros entrada
+			cs.setString(1, u.getEmail());
+			cs.setString(2, u.getPassword());
+			
+			//parametro de salida
+			cs.registerOutParameter(3, Types.INTEGER );
+			
+			int affectedRows = cs.executeUpdate();
 			if (affectedRows == 1) {
+				// conseguir id generado
+				u.setId( cs.getLong(3) );				
 				resul = true;
 			}
 
