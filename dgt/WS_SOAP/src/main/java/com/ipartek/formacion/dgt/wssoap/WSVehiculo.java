b528ade277162afc4d0ede0008dfa7582ecd63e0
anderuraga <ander.uraga@gmail.com>
2019-01-23 14:13:05 +0100

    llamar DAO

--- dgt/WS_SOAP/src/main/java/com/ipartek/formacion/dgt/wssoap/WSVehiculo.java
+++ dgt/WS_SOAP/src/main/java/com/ipartek/formacion/dgt/wssoap/WSVehiculo.java
@@ -1,16 +1,32 @@
 package com.ipartek.formacion.dgt.wssoap;
 
+import com.ipartek.formacion.modelo.daos.CocheDAO;
 import com.ipartek.formacion.modelo.pojo.Coche;
 
 public class WSVehiculo {
 	
 	
+	private static CocheDAO cocheDAO;
+	
+	public WSVehiculo() {
+		super();
+		cocheDAO = CocheDAO.getInstance();
+	}
+
 	public Coche obtenerDatos ( String matricula ) {
 		
-		//TODO llamar DAO
-		Coche c = new Coche();
-		c.setModelo("Mock coche");
-		c.setMatricula("TODO llamar DAO");
+		Coche c = null;
+				
+		if ( matricula != null ) {		
+			c = cocheDAO.getByMatricula(matricula);
+		}	
+		
+		if( c == null ) {
+			c = new Coche();
+			c.setModelo("No se ha encontrado ningun Vehiculo");
+			c.setMatricula(matricula);			
+		}
+		
 		return c;
 	}
 
