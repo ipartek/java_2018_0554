88067dac20db39d6eeebca6415e64e3843c201f2
Curso <Curso@Formacion-TOS12.ipartekaula.com>
2019-01-24 10:27:22 +0100

    proyecto montado de nuevo

--- dgt/modelo/src/main/java/com/ipartek/formacion/modelo/daos/CocheDAO.java
+++ dgt/modelo/src/main/java/com/ipartek/formacion/modelo/daos/CocheDAO.java
@@ -14,53 +14,54 @@ import com.ipartek.formacion.modelo.pojo.Coche;
 public class CocheDAO {
 
 	private final static Logger LOG = Logger.getLogger(CocheDAO.class);
-	private static CocheDAO INSTANCE = null;
-	
-	
+	private static CocheDAO INSTANCE = null;	
 	private static final String SQL_GETMATRICULA = "{call pa_coche_getByMatricula(?)}";
 
-	// constructor privado, solo acceso por getInstance()
+	
 	private CocheDAO() {
 		super();
 	}
 
 	public synchronized static CocheDAO getInstance() {
-
 		if (INSTANCE == null) {
 			INSTANCE = new CocheDAO();
 		}
 		return INSTANCE;
 	}
 
+
+
+	/**
+	 * Obtenemos un Coche por su matricula
+	 * @param matricula
+	 * @return Coche si encuentra, null en caso contrario
+	 */
+	public Coche getByMatricula(String matricula) {
+		Coche c = null;
+		try (Connection conn = ConnectionManager.getConnection();
+				CallableStatement cs = conn.prepareCall(SQL_GETMATRICULA);) {
+
+			cs.setString(1, matricula);
+
+			try (ResultSet rs = cs.executeQuery()) {			
+				while (rs.next()) {
+					c = rowMapper(rs);
+				}		
+			}
+
+		} catch (Exception e) {
+			LOG.error(e);
+		}
+		return c;
+	}
+	
+	
 	private Coche rowMapper(ResultSet rs) throws SQLException {
 		Coche c = new Coche();
 		c.setId(rs.getLong("id"));
 		c.setMatricula(rs.getString("matricula"));
 		c.setModelo(rs.getString("modelo"));
 		c.setKm(rs.getInt("km"));
-		return c;
-	}
-
-	public Coche getByMatricula(String mat) {
-		Coche c = null;
-		try (Connection conn = ConnectionManager.getConnection();
-				CallableStatement cs = conn.prepareCall(SQL_GETMATRICULA);) {
-
-			cs.setString(1, mat);
-
-			try (ResultSet rs = cs.executeQuery()) {
-				try {
-					while (rs.next()) {
-						c = rowMapper(rs);
-					}
-				} catch (Exception e) {					
-					LOG.warn(e);
-				}
-			}
-
-		} catch (Exception e) {
-			LOG.error(e);
-		}
 		return c;
 	}
 }
