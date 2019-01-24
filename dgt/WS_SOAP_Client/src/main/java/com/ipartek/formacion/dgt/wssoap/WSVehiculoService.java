0fa01307531bccc9aca833d10d4cf18667fff8b9
imanol <imahernando@gmail.com>
2019-01-24 11:01:43 +0100

    WebClient Mavenizado

---
+++ dgt/WS_SOAP_Client/src/main/java/com/ipartek/formacion/dgt/wssoap/WSVehiculoService.java
@@ -0,0 +1,16 @@
+/**
+ * WSVehiculoService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.ipartek.formacion.dgt.wssoap;
+
+public interface WSVehiculoService extends javax.xml.rpc.Service {
+    public java.lang.String getWSVehiculoAddress();
+
+    public com.ipartek.formacion.dgt.wssoap.WSVehiculo getWSVehiculo() throws javax.xml.rpc.ServiceException;
+
+    public com.ipartek.formacion.dgt.wssoap.WSVehiculo getWSVehiculo(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
