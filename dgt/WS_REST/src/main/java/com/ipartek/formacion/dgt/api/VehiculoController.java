f0ce2f7bc757d621d666f0d02bc602bdb2049d9c
AnderSolana <andersolanad@gmail.com>
2019-01-25 10:20:37 +0100

    Añadido el detalle de un vehiculo en concreto por su ID

--- dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/VehiculoController.java
+++ dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/VehiculoController.java
@@ -2,9 +2,10 @@ package com.ipartek.formacion.dgt.api;
 
 import java.util.ArrayList;
 import org.apache.log4j.Logger;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ipartek.appMultas.modelo.dao.CocheDAO;
@@ -24,11 +25,14 @@ public class VehiculoController {
 	@RequestMapping( value= {"/api/vehiculo"}, method = RequestMethod.GET)
 	public ArrayList<Coche> listar(){
 		//ArrayList<Coche> coches = new ArrayList<Coche>();
-		
 		//coches.add(new Coche(1L, "BI000CM", "Fiat", 34555L));
 		//coches.add(new Coche(2L, "ZA000HK", "Flagoneta", 34555L));
-		
 		return cocheDao.getAll();
+	}
+	
+	@RequestMapping( value= {"/api/vehiculo/{id}"}, method = RequestMethod.GET)
+	public Coche detalle(@PathVariable Long id) {
+		return cocheDao.getBYId(id);
 	}
 	
 }
