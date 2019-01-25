e2f483f9703c9699a5f14740699732f4eec98629
dZallo <danielzallo@gmail.com>
2019-01-25 13:26:57 +0100

    Delete de un coche

--- dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/VehiculoController.java
+++ dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/VehiculoController.java
@@ -1,17 +1,20 @@
 package com.ipartek.formacion.dgt.api;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ipartek.appMultas.modelo.dao.CocheDAO;
 import com.ipartek.appMultas.modelo.pojo.Coche;
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 
 @RestController
 public class VehiculoController {
@@ -24,19 +27,23 @@ public class VehiculoController {
 		cocheDAO = CocheDAO.getInstance();
 	}
 	
+	/**
+	 * GETALL
+	 * @return
+	 */
 	@RequestMapping(value=("/api/vehiculo"), method=RequestMethod.GET)
 	public ArrayList<Coche> listar(){
-		
-		//ArrayList<Coche> coches = new ArrayList<Coche>();
-		//coches.add(new Coche((long)1, "BI000CM", "Fiat", (long)34555));
-		//coches.add(new Coche((long)2, "ZA000HK", "Flagoneta", (long)34555));
-		
 		return cocheDAO.getAll();
 	}
 	
 	/*
 	 * RequestParam para parametros. ej: ?id=1 @RequestParam(value="id") Long id
 	 * PathVariable para uris. ej: @PathVariable long id
+	 */
+	/**
+	 * GET BY ID
+	 * @param id
+	 * @return
 	 */
 	@RequestMapping(value=("/api/vehiculo/{id}"),method=RequestMethod.GET)
 	public ResponseEntity<Coche> detalle(@PathVariable long id){
@@ -50,5 +57,72 @@ public class VehiculoController {
 		
 		return response;
 	}
+	
+	/**
+	 * INSERT
+	 * @param coche
+	 * @return
+	 */
+	@RequestMapping(value=("/api/vehiculo"), method=RequestMethod.POST)
+	public ResponseEntity<Coche> crear(@RequestBody Coche coche){
+		
+		// TODO realizar crear
+		//404 datos del coche no son correctos, ej sin matricula o vacia
+		//201 creado y retonrar coche con id actualizado
+		//409 ya existe la matricula unique key
+		return new ResponseEntity<Coche>(HttpStatus.NOT_IMPLEMENTED);
+	}
+	
+	/**
+	 * DELETE
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value=("/api/vehiculo/{id}"),method=RequestMethod.DELETE)
+	public ResponseEntity<Coche> eliminar(@PathVariable long id){
+		ResponseEntity<Coche> response = new ResponseEntity<Coche>(HttpStatus.NOT_FOUND);//404
+		
+		try {
+			if(cocheDAO.delete(id)) {
+				response = new ResponseEntity<Coche>(HttpStatus.OK);//200
+			}
+		}catch (MySQLIntegrityConstraintViolationException e) {
+			response = new ResponseEntity<Coche>(HttpStatus.CONFLICT);//409
+			e.printStackTrace();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return response;
+	}
+	
+	/**
+	 * UPDATE
+	 * @param id
+	 * @param coche
+	 * @return
+	 */
+	@RequestMapping(value=("/api/vehiculo/{id}"), method=RequestMethod.PUT)
+	public ResponseEntity<Coche> modificar(
+			@PathVariable long id,
+			@RequestBody Coche coche){
+		// TODO realizar update
+		return new ResponseEntity<Coche>(HttpStatus.NOT_IMPLEMENTED);
+	}
+	
 
+	/**
+	 * UPDATE PARCIAL, fecha baja
+	 * @param id
+	 * @param coche
+	 * @return
+	 */
+	@RequestMapping(value=("/api/vehiculo/{id}"),method=RequestMethod.PATCH)
+	public ResponseEntity<Coche> darDeBaja(@PathVariable long id,@RequestBody Coche coche){
+		
+		// TODO realizar update parcial
+
+		return new ResponseEntity<Coche>(HttpStatus.NOT_IMPLEMENTED);
+	}
+	
+	
 }
