f2fc5bbd5e22da549bfaf9c5aafe776f7037e278
dZallo <danielzallo@gmail.com>
2019-01-25 09:41:15 +0100

    Se ha añadido el Vehiculo Controller y se ha añadido la dependencia del modelo en el pom

---
+++ dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/VehiculoController.java
@@ -0,0 +1,34 @@
+package com.ipartek.formacion.dgt.api;
+
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ipartek.appMultas.modelo.dao.CocheDAO;
+import com.ipartek.appMultas.modelo.pojo.Coche;
+
+@RestController
+public class VehiculoController {
+	
+	private final static Logger LOG = Logger.getLogger(VehiculoController.class);
+	public static CocheDAO cocheDAO;
+	
+	public VehiculoController() {
+		super();
+		cocheDAO = CocheDAO.getInstance();
+	}
+	
+	@RequestMapping(value=("/api/vehiculo"), method=RequestMethod.GET)
+	public ArrayList<Coche> listar(){
+		//TODO hacer metodo getAll de coches
+		ArrayList<Coche> coches = new ArrayList<Coche>();
+		coches.add(new Coche((long)1, "BI000CM", "Fiat", (long)34555));
+		coches.add(new Coche((long)2, "ZA000HK", "Flagoneta", (long)34555));
+		
+		return coches;
+	}
+
+}
