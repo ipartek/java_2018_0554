fee935157e3d5b96f470f120e805b704252137b3
dZallo <danielzallo@gmail.com>
2019-01-25 13:13:49 +0100

    Se han generado los metodos: -insert -delete -updateCoche -darDeBajaCoche

--- dgt/modelo/src/main/java/com/ipartek/appMultas/modelo/dao/CocheDAO.java
+++ dgt/modelo/src/main/java/com/ipartek/appMultas/modelo/dao/CocheDAO.java
@@ -4,6 +4,7 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.ArrayList;
 
 import org.apache.log4j.Logger;
@@ -15,6 +16,11 @@ public class CocheDAO {
 	private final static String SQL_GETBYMATRICULA = "{call coche_getByMatricula(?)}";
 	private final static String SQL_GETBYID = "{call coche_getById(?)}";
 	private final static String SQL_GETALL = "{call coche_getAll()}";
+	private final static String SQL_INSERT = "{call coche_insert(?,?,?,?)}";
+	private final static String SQL_DELETE = "{call coche_delete(?)}";
+	private final static String SQL_UPDATE_COCHE = "{call coche_update(?,?,?,?)}";
+	private final static String SQL_DARDEBAJA_COCHE="{call coche_dar_de_baja(?)}";
+
 
 	private final static Logger LOG = Logger.getLogger(CocheDAO.class);
 	private static CocheDAO INSTANCE = null;
@@ -30,28 +36,6 @@ public class CocheDAO {
 			INSTANCE = new CocheDAO();
 		}
 		return INSTANCE;
-	}
-	/**
-	 * Obtiene un coche a partir de la matrícula que recibe por parámetro. 
-	 * @param matricula Matrícula del coche
-	 * @return Coche con dicha matricula. Null si el coche no existe.
-	 */
-	public Coche getByMatricula(String matricula) {
-		Coche c = null;
-
-		String sql = SQL_GETBYMATRICULA;
-
-		try (Connection conn = ConnectionManager.getConnection(); CallableStatement cs = conn.prepareCall(sql);) {
-			cs.setString(1, matricula);
-			try (ResultSet rs = cs.executeQuery();) {
-				while (rs.next()) {
-					c = rowMapper(rs);
-				}
-			}
-		} catch (Exception e) {
-			LOG.debug(e);
-		}
-		return c;
 	}
 	
 	/**
@@ -81,6 +65,28 @@ public class CocheDAO {
 		return coches;
 	}
 	
+	/**
+	 * Obtiene un coche a partir de la matrícula que recibe por parámetro. 
+	 * @param matricula Matrícula del coche
+	 * @return Coche con dicha matricula. Null si el coche no existe.
+	 */
+	public Coche getByMatricula(String matricula) {
+		Coche c = null;
+
+		String sql = SQL_GETBYMATRICULA;
+
+		try (Connection conn = ConnectionManager.getConnection(); CallableStatement cs = conn.prepareCall(sql);) {
+			cs.setString(1, matricula);
+			try (ResultSet rs = cs.executeQuery();) {
+				while (rs.next()) {
+					c = rowMapper(rs);
+				}
+			}
+		} catch (Exception e) {
+			LOG.debug(e);
+		}
+		return c;
+	}	
 	
 	/**
 	 * Obtiene un coche a partir del ID que recibe por parámetro. 
@@ -103,6 +109,109 @@ public class CocheDAO {
 			LOG.debug(e);
 		}
 		return c;
+	}
+	
+	/**
+	 * Metodo que te permite registrar un nuevo coche
+	 * @param c el objeto coche que se quiere insertar
+	 * @return true si se ha insertado correctamente y false en caso de que no se haya insertado correctamente
+	 * @throws SQLException
+	 */
+	public boolean insert(Coche c) throws SQLException {
+		boolean resul = false;
+		
+		String sql = SQL_INSERT;
+		try(
+			Connection conn = ConnectionManager.getConnection();
+			CallableStatement cs = conn.prepareCall(sql);
+			){
+			//parametros de entrada
+			cs.setString(1, c.getMatricula());
+			cs.setString(2, c.getModelo());
+			cs.setLong(3, c.getKm());
+			
+			// parametros de salida
+			cs.registerOutParameter(4, Types.INTEGER);
+			
+			int affectedRows = cs.executeUpdate();
+			if (affectedRows == 1) {
+				// conseguir id generado
+				c.setId(cs.getLong(4));
+				resul = true;
+			}	
+		}
+		return resul;
+	}
+	
+	/**
+	 * Metodo que elimina un cochede la bd.
+	 * @param idCoche es el id del coche que se quiere eliminar
+	 * @return
+	 * @throws SQLException
+	 */
+	public boolean delete(Long idCoche) throws SQLException {
+		boolean resul= false;
+		String sql = SQL_DELETE;
+		try(
+			Connection conn = ConnectionManager.getConnection();
+			CallableStatement cs = conn.prepareCall(sql);
+			){
+			cs.setLong(1, idCoche);
+			int affectedRows = cs.executeUpdate();
+			if (affectedRows == 1) {
+				resul = true;
+			}
+		}
+		return resul;
+	}
+	
+	/**
+	 * Metodo que actualiza todo el coche
+	 * @param c
+	 * @return
+	 * @throws SQLException
+	 */
+	public boolean updateCoche(Coche c) throws SQLException {
+		boolean resul = false;
+		String sql = SQL_UPDATE_COCHE;
+		try(Connection conn = ConnectionManager.getConnection();
+			CallableStatement cs = conn.prepareCall(sql);
+			){
+			cs.setString(1, c.getMatricula());
+			cs.setString(2, c.getModelo());
+			cs.setLong(3, c.getKm());
+			cs.setLong(4, c.getId());
+			
+			int affectedRows = cs.executeUpdate();
+			if (affectedRows == 1) {
+				resul = true;
+			}	
+		}
+		
+		return resul;
+	}
+	
+	/**
+	 * Metodo que anula un coche, actualizando su campo fecha_baja en la bd
+	 * @param idCoche el id del coche que se quiere anular
+	 * @return 
+	 * @throws SQLException
+	 */
+	public boolean darDeBajaCoche(Long idCoche) throws SQLException {
+		boolean resul= false;
+		String sql = SQL_DARDEBAJA_COCHE;
+		
+		try(
+			Connection conn = ConnectionManager.getConnection();
+			CallableStatement cs = conn.prepareCall(sql);
+			){
+			cs.setLong(1, idCoche);
+			int affectedRows = cs.executeUpdate();
+			if (affectedRows == 1) {
+				resul = true;
+			}
+		}
+		return resul;
 	}
 	/**
 	 * Constructor del coche mediante una consulta a la BD. 
