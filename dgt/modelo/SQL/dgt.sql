2ff8de64e3db3f2cdad5a7cbb27504e98c1a0d1a
AnderSolana <andersolanad@gmail.com>
2019-01-25 10:20:23 +0100

    Actualizada la BD con el procedure getAll

--- dgt/modelo/SQL/dgt.sql
+++ dgt/modelo/SQL/dgt.sql
@@ -2,7 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `dgt` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `dgt`;
 -- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
 --
--- Host: localhost    Database: dgt
+-- Host: 127.0.0.1    Database: dgt
 -- ------------------------------------------------------
 -- Server version	8.0.12
 
@@ -45,25 +45,6 @@ LOCK TABLES `agente` WRITE;
 INSERT INTO `agente` VALUES (1,'Majonei','1234',39,'12345678'),(2,'Jonny Walker','4321',36,'12345678'),(3,'Monk','7890',38,'12345678'),(4,'Takel Berry','0987',37,'12345678'),(5,'Tontini','5678',37,'12345678');
 /*!40000 ALTER TABLE `agente` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `tbi_agente_check` BEFORE INSERT ON `agente` FOR EACH ROW BEGIN
-	SET NEW.nombre = TRIM(NEW.nombre);
-    SET NEW.placa = TRIM(NEW.placa);
-    SET NEW.password = TRIM(NEW.password);
-END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `coche`
@@ -91,24 +72,6 @@ LOCK TABLES `coche` WRITE;
 INSERT INTO `coche` VALUES (1,'3548MKZ','toyota yaris',500),(3,'9605EFH','Fiat Multipla',800),(4,'5674MBD','GRT',1800),(6,'BI0020AZ','flagoneta',47500);
 /*!40000 ALTER TABLE `coche` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `tbi_coche_check` BEFORE INSERT ON `coche` FOR EACH ROW BEGIN
-	SET NEW.matricula = TRIM(NEW.matricula);
-    SET NEW.modelo = TRIM(NEW.modelo);
-END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `departamento`
@@ -133,23 +96,6 @@ LOCK TABLES `departamento` WRITE;
 INSERT INTO `departamento` VALUES (1,'IT'),(2,'DEV'),(31,'VENTAS'),(33,'Ingeniería'),(34,'Producción'),(35,'Mercadeo'),(36,'Alcoholemia'),(37,'Oficina'),(38,'Fealdad'),(39,'Velocidad');
 /*!40000 ALTER TABLE `departamento` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `tbi_departamento_check` BEFORE INSERT ON `departamento` FOR EACH ROW BEGIN
-	SET NEW.nombre = TRIM(NEW.nombre);
-END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `multa`
@@ -172,7 +118,7 @@ CREATE TABLE `multa` (
   KEY `fk_multas_agentes_idx` (`id_agente`),
   CONSTRAINT `fk_multas_agentes` FOREIGN KEY (`id_agente`) REFERENCES `agente` (`id`),
   CONSTRAINT `fk_multas_coches` FOREIGN KEY (`id_coche`) REFERENCES `coche` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -181,30 +127,9 @@ CREATE TABLE `multa` (
 
 LOCK TABLES `multa` WRITE;
 /*!40000 ALTER TABLE `multa` DISABLE KEYS */;
-INSERT INTO `multa` VALUES (2,6,1,500,'exceso velocidad 240km/h','2018-01-07 10:38:46','2018-01-07 10:38:46',NULL),(5,1,1,1400,'por ir a200k/h en zona de 30km/h','2018-02-07 12:40:21','2018-02-07 12:40:21',NULL),(7,1,1,500,'Pasar semáforo en rojo','2019-01-09 13:25:34','2019-01-14 12:49:19','2019-01-14 12:49:19'),(9,6,1,1000,'Aparcar donde no debe','2019-01-09 13:29:00','2019-01-18 12:05:47',NULL),(17,1,1,500,'Ha excedido el límite de velocidad, 20km/h','2019-01-10 12:12:51','2019-01-18 12:06:57',NULL),(22,6,1,50,'Iba 20km/h por encima en una zona de 120','2019-01-14 12:51:03','2019-01-14 12:59:34','2019-01-14 12:59:34'),(24,6,1,200,'Se ha saltado un semaforo en rojo','2019-01-14 13:09:16','2019-01-18 12:04:28','2019-01-18 12:04:28'),(28,1,1,222,'Se ha saltado un Stop\r\n','2019-01-15 10:55:27','2019-01-15 10:58:25','2019-01-15 10:58:25'),(29,1,1,2000,'Stop y semaforo en rojo','2019-01-15 11:36:51','2019-01-15 11:36:57','2019-01-15 11:36:57'),(30,1,1,250,'Por aparcar en zona de carga-descarga','2019-01-16 09:21:31','2019-01-16 09:23:24','2019-01-16 09:23:24'),(31,4,2,100,'Se ha saltado un semaforo en rojo','2018-01-16 12:07:30','2018-01-16 12:07:30',NULL),(32,1,2,100,'Se ha saltado un semaforo en rojo','2019-01-16 12:14:42','2019-01-16 13:32:41','2019-01-16 13:32:41'),(33,3,2,50,'Ha excedido el límite de velocidad 18Km/h por encima del límite','2019-01-16 12:14:42','2019-01-16 12:14:42',NULL),(34,4,3,100,'Se ha saltado un semaforo en rojo','2019-01-16 12:16:30','2019-01-16 12:16:30',NULL),(35,3,3,150,'Ha excedido el límite de velocidad 25Km/h por encima del límite','2019-01-16 12:16:30','2019-01-16 12:16:30',NULL),(36,3,3,25,'No ha cedido el paso a los peatones en ningun paso de cebra','2019-01-16 12:16:30','2019-01-16 12:16:30',NULL),(37,4,4,74.95,'Se ha saltado un STOP','2019-01-16 12:33:21','2019-01-16 12:33:21',NULL),(38,4,4,530,'Ha doblado la tasa de alcoholemia permitida','2019-01-16 12:33:21','2019-01-16 12:33:21',NULL),(39,3,4,75,'El coche estaba circulando sin tener la ITV y/o un permiso especial','2019-01-16 12:33:21','2019-01-16 12:33:21',NULL),(40,4,5,74.95,'Se ha saltado un STOP','2019-01-16 12:41:07','2019-01-16 12:41:07',NULL),(41,6,5,200,'Ha superado la tasa de alcoholemia permitida por 0.2','2019-01-16 12:41:07','2019-01-16 12:41:07',NULL),(42,3,5,150,'El coche estaba circulando sin tener seguro','2019-01-16 12:41:07','2019-01-16 12:41:07',NULL),(43,1,1,50,'No ha marcado la trayectoria que iba a tomar con el intermitente','2019-01-16 13:06:37','2019-01-16 13:07:28','2019-01-16 13:07:28'),(44,1,4,50,'Se ha saltado un semáforo poniendo en peligro a los demás usuarios','2019-01-16 13:58:48','2019-01-16 13:59:33',NULL),(45,4,1,200,'Ha dado positivo en el control de alcoholemia realizado','2019-01-16 14:08:16','2019-01-16 14:08:58','2019-01-16 14:08:58'),(46,4,2,300,'Te has saltado el último STOP','2018-01-16 14:08:16','2018-01-16 14:08:16',NULL),(47,1,1,300,'Se ha saltado un STOP','2019-01-18 11:52:57','2019-01-18 12:04:19','2019-01-18 12:04:19'),(48,1,1,222.2,'Te has saltado un STOP','2019-01-18 12:06:47','2019-01-18 12:06:47',NULL),(49,1,1,200,'Se ha saltado un STOP','2018-12-21 10:30:58','2018-12-21 10:30:58',NULL),(50,1,1,250.5,'Se ha saltado un STOP','2018-11-21 10:30:58','2018-11-21 10:30:58',NULL),(51,1,1,100,'Se ha saltado un STOP','2018-10-21 10:30:58','2018-10-21 10:30:58',NULL),(52,1,1,12.2,'Se ha saltado un STOP','2018-09-21 10:30:58','2018-09-21 10:30:58',NULL),(53,1,1,300,'Se ha saltado un STOP','2018-08-21 10:30:58','2018-08-21 10:30:58',NULL),(54,1,1,234,'Se ha saltado un STOP','2018-07-21 10:30:58','2018-07-21 10:30:58',NULL),(55,1,1,12,'Se ha saltado un STOP','2018-06-21 10:30:58','2018-06-21 10:30:58',NULL),(56,1,1,654,'Se ha saltado un STOP','2018-05-21 10:30:58','2018-05-21 10:30:58',NULL),(57,1,1,23,'Se ha saltado un STOP','2018-04-21 10:30:58','2018-04-21 10:30:58',NULL),(58,1,1,435,'Se ha saltado un STOP','2018-03-21 10:30:58','2018-03-21 10:30:58',NULL);
+INSERT INTO `multa` VALUES (2,6,1,500,'exceso velocidad 240km/h','2018-01-07 10:38:46','2018-01-07 10:38:46',NULL),(5,1,1,1400,'por ir a200k/h en zona de 30km/h','2018-02-07 12:40:21','2018-02-07 12:40:21',NULL),(7,1,1,500,'Pasar semáforo en rojo','2019-01-09 13:25:34','2019-01-14 12:49:19','2019-01-14 12:49:19'),(9,6,1,1000,'Aparcar donde no debe','2019-01-09 13:29:00','2019-01-18 12:05:47',NULL),(17,1,1,500,'Ha excedido el límite de velocidad, 20km/h','2019-01-10 12:12:51','2019-01-18 12:06:57',NULL),(22,6,1,50,'Iba 20km/h por encima en una zona de 120','2019-01-14 12:51:03','2019-01-14 12:59:34','2019-01-14 12:59:34'),(24,6,1,200,'Se ha saltado un semaforo en rojo','2019-01-14 13:09:16','2019-01-18 12:04:28','2019-01-18 12:04:28'),(28,1,1,222,'Se ha saltado un Stop\r\n','2019-01-15 10:55:27','2019-01-15 10:58:25','2019-01-15 10:58:25'),(29,1,1,2000,'Stop y semaforo en rojo','2019-01-15 11:36:51','2019-01-15 11:36:57','2019-01-15 11:36:57'),(30,1,1,250,'Por aparcar en zona de carga-descarga','2019-01-16 09:21:31','2019-01-16 09:23:24','2019-01-16 09:23:24'),(31,4,2,100,'Se ha saltado un semaforo en rojo','2018-01-16 12:07:30','2018-01-16 12:07:30',NULL),(32,1,2,100,'Se ha saltado un semaforo en rojo','2019-01-16 12:14:42','2019-01-16 13:32:41','2019-01-16 13:32:41'),(33,3,2,50,'Ha excedido el límite de velocidad 18Km/h por encima del límite','2019-01-16 12:14:42','2019-01-16 12:14:42',NULL),(34,4,3,100,'Se ha saltado un semaforo en rojo','2019-01-16 12:16:30','2019-01-16 12:16:30',NULL),(35,3,3,150,'Ha excedido el límite de velocidad 25Km/h por encima del límite','2019-01-16 12:16:30','2019-01-16 12:16:30',NULL),(36,3,3,25,'No ha cedido el paso a los peatones en ningun paso de cebra','2019-01-16 12:16:30','2019-01-16 12:16:30',NULL),(37,4,4,74.95,'Se ha saltado un STOP','2019-01-16 12:33:21','2019-01-16 12:33:21',NULL),(38,4,4,530,'Ha doblado la tasa de alcoholemia permitida','2019-01-16 12:33:21','2019-01-16 12:33:21',NULL),(39,3,4,75,'El coche estaba circulando sin tener la ITV y/o un permiso especial','2019-01-16 12:33:21','2019-01-16 12:33:21',NULL),(40,4,5,74.95,'Se ha saltado un STOP','2019-01-16 12:41:07','2019-01-16 12:41:07',NULL),(41,6,5,200,'Ha superado la tasa de alcoholemia permitida por 0.2','2019-01-16 12:41:07','2019-01-16 12:41:07',NULL),(42,3,5,150,'El coche estaba circulando sin tener seguro','2019-01-16 12:41:07','2019-01-16 12:41:07',NULL),(43,1,1,50,'No ha marcado la trayectoria que iba a tomar con el intermitente','2019-01-16 13:06:37','2019-01-16 13:07:28','2019-01-16 13:07:28'),(44,1,4,50,'Se ha saltado un semáforo poniendo en peligro a los demás usuarios','2019-01-16 13:58:48','2019-01-16 13:59:33',NULL),(45,4,1,200,'Ha dado positivo en el control de alcoholemia realizado','2019-01-16 14:08:16','2019-01-16 14:08:58','2019-01-16 14:08:58'),(46,4,2,300,'Te has saltado el último STOP','2018-01-16 14:08:16','2018-01-16 14:08:16',NULL),(47,1,1,300,'Se ha saltado un STOP','2019-01-18 11:52:57','2019-01-18 12:04:19','2019-01-18 12:04:19'),(48,1,1,222.2,'Te has saltado un STOP','2019-01-18 12:06:47','2019-01-18 12:06:47',NULL),(49,1,1,200,'Se ha saltado un STOP','2018-12-21 10:30:58','2018-12-21 10:30:58',NULL),(50,1,1,250.5,'Se ha saltado un STOP','2018-11-21 10:30:58','2018-11-21 10:30:58',NULL),(51,1,1,100,'Se ha saltado un STOP','2018-10-21 10:30:58','2018-10-21 10:30:58',NULL),(52,1,1,12.2,'Se ha saltado un STOP','2018-09-21 10:30:58','2018-09-21 10:30:58',NULL),(53,1,1,300,'Se ha saltado un STOP','2018-08-21 10:30:58','2018-08-21 10:30:58',NULL),(54,1,1,234,'Se ha saltado un STOP','2018-07-21 10:30:58','2018-07-21 10:30:58',NULL),(55,1,1,12,'Se ha saltado un STOP','2018-06-21 10:30:58','2018-06-21 10:30:58',NULL),(56,1,1,654,'Se ha saltado un STOP','2018-05-21 10:30:58','2018-05-21 10:30:58',NULL),(57,1,1,23,'Se ha saltado un STOP','2018-04-21 10:30:58','2018-04-21 10:30:58',NULL),(58,1,1,435,'Se ha saltado un STOP','2018-03-21 10:30:58','2018-03-21 10:30:58',NULL),(59,6,1,69.55,'Pruebas de multar con decimilaessssssssss','2019-01-23 08:43:52','2019-01-23 08:44:36',NULL);
 /*!40000 ALTER TABLE `multa` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `tbi_multa_check` BEFORE INSERT ON `multa` FOR EACH ROW BEGIN
-	IF NEW.importe < 0 THEN
-		SET NEW.importe=1;
-	END IF;
-    
-    SET NEW.concepto = TRIM(NEW.concepto);
-END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Temporary view structure for view `v_objetivos`
@@ -257,10 +182,6 @@ SET character_set_client = utf8mb4;
 SET character_set_client = @saved_cs_client;
 
 --
--- Dumping events for database 'dgt'
---
-
---
 -- Dumping routines for database 'dgt'
 --
 /*!50003 DROP PROCEDURE IF EXISTS `agente_getById` */;
@@ -295,6 +216,25 @@ DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `agente_login`(IN p_numplaca VARCHAR(45), IN p_password VARCHAR(50))
 BEGIN
 	SELECT id, nombre, placa, id_departamento, `password` FROM agente WHERE placa = p_numplaca AND `password` = p_password;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `coche_getAll` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `coche_getAll`()
+BEGIN
+	SELECT id, matricula, modelo, km FROM coche;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -606,4 +546,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-21 10:37:27
+-- Dump completed on 2019-01-25 10:19:40
