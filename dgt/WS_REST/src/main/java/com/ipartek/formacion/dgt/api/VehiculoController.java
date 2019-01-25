49bac9d8b9ebcd079d31159503403d1b8ecc0e82
dZallo <danielzallo@gmail.com>
2019-01-25 10:36:45 +0100

    Se ha generado el metodo detalle que busca un coche mediante su id y le devuelve un ResponseEntity--> el cual tiene el coche en null sino lo encuentra y un status code de Not Found. En caso de existe le enviara el coche obtenido y un status code de OK

--- dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/VehiculoController.java
+++ dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/VehiculoController.java
@@ -3,6 +3,9 @@ package com.ipartek.formacion.dgt.api;
 import java.util.ArrayList;
 
 import org.apache.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +32,23 @@ public class VehiculoController {
 		//coches.add(new Coche((long)2, "ZA000HK", "Flagoneta", (long)34555));
 		
 		return cocheDAO.getAll();
+	}
+	
+	/*
+	 * RequestParam para parametros. ej: ?id=1 @RequestParam(value="id") Long id
+	 * PathVariable para uris. ej: @PathVariable long id
+	 */
+	@RequestMapping(value=("/api/vehiculo/{id}"),method=RequestMethod.GET)
+	public ResponseEntity<Coche> detalle(@PathVariable long id){
+		Coche c = null;
+		ResponseEntity<Coche> response = new ResponseEntity<Coche>(c,HttpStatus.NOT_FOUND);
+		
+		if(id>0) {
+			c= cocheDAO.getBYId(id);
+			response = new ResponseEntity<Coche>(c,HttpStatus.OK);
+		}
+		
+		return response;
 	}
 
 }
