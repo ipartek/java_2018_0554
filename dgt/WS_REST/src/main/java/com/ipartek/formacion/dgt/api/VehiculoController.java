b562ef57f64a54e9392077b157b4186e29373e84
anderuraga <ander.uraga@gmail.com>
2019-01-25 14:22:31 +0100

    Habilitar CORS

--- dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/VehiculoController.java
+++ dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/VehiculoController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ipartek.formacion.modelo.daos.CocheDAO;
 import com.ipartek.formacion.modelo.pojo.Coche;
 
+@CrossOrigin
 @RestController
 public class VehiculoController {
 
