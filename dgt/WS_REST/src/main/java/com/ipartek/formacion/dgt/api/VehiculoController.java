414f6c8f695be13c5518ded1166ce7895973aa05
anderuraga <ander.uraga@gmail.com>
2019-01-25 10:39:40 +0100

    Detalle coche 200 y 404

--- dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/VehiculoController.java
+++ dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/VehiculoController.java
@@ -3,6 +3,9 @@ package com.ipartek.formacion.dgt.api;
 import java.util.ArrayList;
 
 import org.apache.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +34,25 @@ public class VehiculoController {
 		
 		return coches;
 	}
+	
+	
+	@RequestMapping( value= {"/api/vehiculo/{id}"}, method = RequestMethod.GET)
+	public ResponseEntity<Coche> detalle( @PathVariable long id ){		
+		
+		ResponseEntity<Coche> response = new ResponseEntity<Coche>( HttpStatus.NOT_FOUND );
+		
+		if ( id > 0 ) {
+			
+			Coche c = new Coche( 2L, "ZA000HK", "Flagoneta", 34555 );
+			c.setId(id);
+			response = new ResponseEntity<Coche>( c, HttpStatus.OK );			
+		}
+			 	
+		return response;
+	}
+	
+	
+	
 	
 	
 	
