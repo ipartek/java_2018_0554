b13d8d95239cc98b55bbbd0e4344f0ba14c5516b
anderuraga <ander.uraga@gmail.com>
2019-01-25 11:52:34 +0100

    TODO metodos sin implementar

--- dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/VehiculoController.java
+++ dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/VehiculoController.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +53,48 @@ public class VehiculoController {
 	}
 	
 	
+	@RequestMapping( value= {"/api/vehiculo/{id}"}, method = RequestMethod.DELETE)
+	public ResponseEntity<Coche> eliminar( @PathVariable long id ){		
+		
+		ResponseEntity<Coche> response = new ResponseEntity<Coche>( HttpStatus.NOT_FOUND );
+		
+		if ( id > 0 ) {
+			
+			response = new ResponseEntity<Coche>(HttpStatus.OK );	
+			
+			//TODO 409 cuando no se pueda eliminar Coche porque tiene multas asociadas
+			
+		}
+			 	
+		return response;
+	}
+	
+	
+	@RequestMapping( value= {"/api/vehiculo/"}, method = RequestMethod.POST)
+	public ResponseEntity<Coche> crear( @RequestBody Coche coche ){
+		
+		//TODO terminar		
+		// 201 creado y retornar coche con id actulizado		
+		// 400 datos del coche no son correctos, por ejemplo sin matricula o vacia		
+		// 409 ya exsite la matricula Unique Key 		
+		return new ResponseEntity<Coche>( HttpStatus.NOT_IMPLEMENTED );		
+	}
+	
+	@RequestMapping( value= {"/api/vehiculo/{id}"}, method = RequestMethod.PUT)
+	public ResponseEntity<Coche> modificar(  
+				@PathVariable long id, 
+				@RequestBody Coche coche ){
+		
+		//TODO terminar
+		return new ResponseEntity<Coche>( HttpStatus.NOT_IMPLEMENTED );		
+	}
+	
+	@RequestMapping( value= {"/api/vehiculo/{id}"}, method = RequestMethod.PATCH)
+	public ResponseEntity<Coche> darDeBaja( @PathVariable long id,  @RequestBody Coche coche ){		
+		
+		//TODO terminar
+		return new ResponseEntity<Coche>( HttpStatus.NOT_IMPLEMENTED );
+	}
 	
 	
 	
