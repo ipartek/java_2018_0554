530f2f4dfc5f6497c8d7ff00a68d1d6f6edf0498
anderuraga <ander.uraga@gmail.com>
2019-01-28 13:44:57 +0100

    crear coche TODO mapear campos

--- dgt/WS_REST_Client_JS/js/main.js
+++ dgt/WS_REST_Client_JS/js/main.js
@@ -31,13 +31,13 @@ function refrescarLista(){
         console.debug('tenemos los datos %o', data);
         vehiculos = data;
         let lis = '';
-        vehiculos.forEach( el => {
+        vehiculos.forEach( (el,pos) => {
             console.debug(el);
             lis += `<li class="list-group-item">
                         <span class="matricula">${el.matricula}</span> 
                         <span class="modelo">${el.modelo}</span>
                         <span class="km">${el.km} KM</span>
-                        <a href="#">Eliminar</a>
+                        <a href="#" onclick="eliminar(${pos})">Eliminar</a>
                     </li>`;
         });
         ulVehiculos.innerHTML = lis;
@@ -45,6 +45,47 @@ function refrescarLista(){
     }).catch(e => {
         console.error(' tenemos un error ' + e);
     });
-  
+} // refrescarLista
 
-}
+
+function eliminar( posicion ){
+
+    let vehiculo = vehiculos[posicion];
+    console.log('click Eliminar %o', vehiculo );
+
+    if ( confirm('¿Estas Seguro?') ){
+        let promesa = ajax('DELETE',`http://localhost:8080/wsrest/api/vehiculo/${vehiculo.id}`);
+        promesa.then( data =>{
+            console.debug('Vehiculo eliminado');
+            refrescarLista();
+        }).catch( e => {
+            console.warn('Vehiculo NO eliminado %o', e);
+        });
+    }    
+    
+} // eliminar
+
+
+function crear(){
+    console.log('click crear' );
+
+    let matricula = document.getElementById('matricula').value;
+
+    let jsonCoche = {
+        "matricula" : matricula,
+        "modelo": "harcodeado",
+        "km": 1000000
+    };
+
+   let xhr = new XMLHttpRequest();    
+   xhr.onreadystatechange = function(){ 
+        if (xhr.readyState == 4 ){   
+            console.debug(' STATUS ' + xhr.status );
+            refrescarLista();
+        }    
+   };
+   xhr.open('POST', 'http://localhost:8080/wsrest/api/vehiculo/');
+   xhr.setRequestHeader("Content-type", "application/json");
+   xhr.send( JSON.stringify(jsonCoche) );
+
+};
