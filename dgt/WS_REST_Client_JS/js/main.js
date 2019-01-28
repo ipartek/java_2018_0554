7996230ac43117560ef5e6b764f3e41b07aa935f
dZallo <danielzallo@gmail.com>
2019-01-28 14:29:45 +0100

    Cliente js la cual obtiene de la api rest

--- dgt/WS_REST_Client_JS/js/main.js
+++ dgt/WS_REST_Client_JS/js/main.js
@@ -1,33 +1,72 @@
-console.log("ready");
+//variables globales
+var ulVehiculos;
+var vehiculos = [];
+var vehiculoSeleccionado;
 
-var request = new XMLHttpRequest(); //ajax
-request.onreadystatechange = function() {
-    if(request.readyState === 4) {
-      
-    }
-      if(request.status === 200) { 
-       console.log("200");
-       // es texto
-       console.log(request.responseText); 
-        //es json, hay que parsearlo de text a json
-       let vehiculos = JSON.parse(request.responseText); 
-       console.log(vehiculos);
+window.addEventListener('load',function(){
+    console.info('documento cargado y listo');
+    // resto de llamadas a metodos
+    ulVehiculos= document.getElementById('ulVehiculo');
 
-        //vaciar lista
-        let ul = document.getElementById('ulVehiculos');
-        ul.innerHTML = '';
+    refrescarLista();
+    
+});
 
-        //recorrer vehiculos
-        let lis='';
-        vehiculos.forEach(el=> {
-           lis +=`<li class="list-group-item">
-           ${el.matricula}  ${el.modelo} ${el.km}
-                    </li>`;
+function  refrescarLista(){
+    console.trace('refrescarLista');
+    ulVehiculos.innerHTML ='<li class="list-group-item">Cargando vehículos ....</li>';
+
+    let promesa = ajax('GET','http://localhost:8080/wsrest/api/vehiculo/');
+    promesa.then(data =>{
+        console.log('tenemos los datos %o', data);
+        vehiculos = data;
+        let lis = '';
+        vehiculos.forEach((el,i) => {
+           console.debug(el); 
+           lis +=`
+           <li class="list-group-item">
+            <span class="matricula"> ${el.matricula}</span> 
+            <span class="modelo"> ${el.modelo} </span>
+            <span class="km">${el.km}</span>
+            <a href="#" onclick="eliminar(${i})">Eliminar</a>
+            </li>`;
         });
+        ulVehiculos.innerHTML=lis;
+    }).catch(e =>{
+        console.error('Tenemos un error '+ e);
+    });
+}
 
-        ul.innerHTML=lis;
-
+function eliminar(i){
+    let vehiculo = vehiculos[i];
+    console.log('click Eliminar %o',vehiculo.id);
+    if(confirm('¿Estás seguro?')){
+        let promesa = ajax('DELETE',`http://localhost:8080/wsrest/api/vehiculo/${vehiculo.id}`);
+        promesa.then(data=>{
+            console.log('Vehiculo eliminado');
+            refrescarLista();
+        }).catch(e=>{
+            console.error('Tenemos un error '+ e);
+        });
     }
-  }
-  request.open('GET', 'http://localhost:8080/wsrest/api/vehiculo');
-  request.send();
+
+    function crear(){
+        console.log('click crear')
+
+        let matricula = documente.getElementById('matricula').value;
+
+        let json = {
+            "matricula" : matricula,
+            "modelo" : "harcodeado",
+            "km" : 10000
+        };
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange= function(){
+            if(request.readyState == 4){
+                console.debug('STATUS' + xhr.status);
+            }
+        };
+        xhr.open('POST', 'http://localhost:8080/wsrest/api/vehiculo/', true);
+        request.send( JSON.stringify(data) );
+    }
+}
