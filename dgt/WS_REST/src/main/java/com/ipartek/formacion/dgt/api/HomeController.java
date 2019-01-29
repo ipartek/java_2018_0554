a35b6ffedf60949ff27aee9d58e2fe28b3463a3e
anderuraga <ander.uraga@gmail.com>
2019-01-29 12:01:09 +0100

    ignorar en la documentacion de Swagger

--- dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/HomeController.java
+++ dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/HomeController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import springfox.documentation.annotations.ApiIgnore;
+
+@ApiIgnore
 @Controller
 public class HomeController {
 	
