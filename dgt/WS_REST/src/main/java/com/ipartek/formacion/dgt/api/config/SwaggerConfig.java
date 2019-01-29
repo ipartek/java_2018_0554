070f8e86f8bb73ec5d8dd593d64c42fd11f91e08
dZallo <danielzallo@gmail.com>
2019-01-29 11:56:42 +0100

    Configuración de Swagger

---
+++ dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.ipartek.formacion.dgt.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@ComponentScan
+@EnableSwagger2
+@EnableWebMvc
+public class SwaggerConfig {
+	
+	@Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build().apiInfo(apiInfo())
+                .useDefaultResponseMessages(false);
+    }
+     
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo("Dirección general de Tráfico API",
+                                        "",
+                                        "1.1",
+                                        "",
+                                        new Contact("Daniel Zallo", "https://github.com/ipartek/java_2018_0554/tree/danielzallo/dgt/WS_REST", "danielzallo@gmail.com"),
+                                        "Apache License",
+                                        "");
+        return apiInfo;
+    }
+
+}
