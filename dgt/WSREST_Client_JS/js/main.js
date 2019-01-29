380f9dd21f6c32dc4243290764624e941b91d338
AnderSolana <Curso@IPARTEKAULA.COM>
2019-01-29 09:52:06 +0100

    guardar

--- dgt/WSREST_Client_JS/js/main.js
+++ dgt/WSREST_Client_JS/js/main.js
@@ -1,112 +1,112 @@
-
 //variables globales
 var ulVehiculos;
 var vehiculos = [];
 var vehiculoSeleccionado;
 
-const ENDPOINT = 'http://localhost:8080/wsrest/api/vehiculo/';
+const ENDPOINT = "http://localhost:8080/wsrest/api/vehiculo/";
 
+window.addEventListener("load", function() {
+  console.info("documento cargado y listo");
+  // console.trace('esto es un trace');
+  // console.debug('esto es un debug');
+  // console.warn('esto es un waring');
+  // console.error('esto es un ERROR');
 
-window.addEventListener('load', function(){
+  ulVehiculos = document.getElementById("ulVehiculo");
 
-    console.info('documento cargado y listo');    
-    // console.trace('esto es un trace');
-    // console.debug('esto es un debug');
-    // console.warn('esto es un waring');
-    // console.error('esto es un ERROR');
-    
-    ulVehiculos = document.getElementById('ulVehiculo');
-
-    refrescarLista();
-
+  refrescarLista();
 });
 
+function refrescarLista() {
+  console.trace("refrescarLista");
+  ulVehiculos.innerHTML =
+    '<li class="list-group-item">Cargando vehiculos....</li>';
 
-function refrescarLista(){
-
-    console.trace('refrescarLista');
-    ulVehiculos.innerHTML = '<li class="list-group-item">Cargando vehiculos....</li>';
-
-    let xhr = new XMLHttpRequest();    
-    xhr.onreadystatechange = function(){ 
-         if (xhr.readyState == 4 && xhr.status == 200 ){               
-            let lis = '';
-            vehiculos = JSON.parse(xhr.responseText);
-            vehiculos.forEach( (vehiculo, index) => {
-                lis += ` <li class="list-group-item">
-                            <span class="matricula">${vehiculo.matricula}</span> 
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      let lis = "";
+      vehiculos = JSON.parse(xhr.responseText);
+      vehiculos.forEach((vehiculo, index) => {
+        lis += ` <li class="list-group-item">
+                            <span class="matricula">${
+                              vehiculo.matricula
+                            }</span> 
                             <span class="modelo">${vehiculo.modelo}</span>
                             <span class="km"> ${vehiculo.km} KM</span>
-                            <a href="#" onclick="eliminar(${vehiculo.id})">Eliminar</a>
+                            <a href="#" onclick="eliminar(${
+                              vehiculo.id
+                            })">Eliminar</a>
                         </li>`;
-            });
-            ulVehiculos.innerHTML = lis;
-         }    
-    };
-    xhr.open('GET', ENDPOINT );    
-    xhr.send();
+      });
+      ulVehiculos.innerHTML = lis;
+    }
+  };
+  xhr.open("GET", ENDPOINT);
+  xhr.send();
 } // refrescarLista
 
+function eliminar(idVehiculo) {
+  console.log("click Eliminar %o", idVehiculo);
 
-function eliminar( idVehiculo ){
-
-   
-    console.log('click Eliminar %o', idVehiculo );
-
-    if ( confirm('¿Estas Seguro?') ){
-        let xhr = new XMLHttpRequest();    
-        xhr.onreadystatechange = function(){ 
-             if (xhr.readyState == 4) {
-                 if ( xhr.status == 200 ){               
-                    refrescarLista();
-                 }else if ( xhr.status == 409 ){
-                    showAlert('CONFLICTO existen multas asociadas', 'warning');
-                 }   
-             }    
-        };
-        xhr.open('DELETE', ENDPOINT + idVehiculo );    
-        xhr.send();
-    }    
-    
+  if (confirm("¿Estas Seguro?")) {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          refrescarLista();
+        } else if (xhr.status == 409) {
+          showAlert("CONFLICTO: Existen multas asociadas", "warning");
+        }
+      }
+    };
+    xhr.open("DELETE", ENDPOINT + idVehiculo);
+    xhr.send();
+  }
 } // eliminar
 
+function crear() {
+  console.log("click crear");
 
-function crear(){
-    console.log('click crear' );
+  let matricula = document.getElementById("matricula").value;
+  let modelo = document.getElementById("modelo").value;
+  let km = document.getElementById("km").value;
 
-    let matricula = document.getElementById('matricula').value;
+  let jsonCoche = {
+    matricula: matricula,
+    modelo: modelo,
+    km: km
+  };
 
-    let jsonCoche = {
-        "matricula" : matricula,
-        "modelo": "harcodeado",
-        "km": 1000000
-    };
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          showAlert("Vehículo creado.", "success");
+          refrescarLista();
+        } else if (xhr.status == 409) {
+          showAlert("CONFLICTO: Esa matrícula ya existe en la DB.", "warning");
+        } else if (xhr.status == 400) {
+          showAlert("ERROR: El formato de los campos introducidos no es correcto.", "danger");
+        }
+      }
+      console.debug(" STATUS " + xhr.status);
+      refrescarLista();
+    }
+  };
+  xhr.open("POST", ENDPOINT);
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.send(JSON.stringify(jsonCoche));
+}
 
-   let xhr = new XMLHttpRequest();    
-   xhr.onreadystatechange = function(){ 
-        if (xhr.readyState == 4 ){   
-            console.debug(' STATUS ' + xhr.status );
-            refrescarLista();
-        }    
-   };
-   xhr.open('POST', ENDPOINT );
-   xhr.setRequestHeader("Content-type", "application/json");
-   xhr.send( JSON.stringify(jsonCoche) );
-
-};
-
-
-function showAlert( texto, tipo = 'info' ){
-
-    
-   let alertHTML = `<div class="alert alert-${tipo} alert-dismissible fade show" role="alert">
+function showAlert(texto, tipo = "info") {
+  let alertHTML = `<div class="alert alert-${tipo} alert-dismissible fade show" role="alert">
         <p>${texto}</p>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
         </div>`;
 
-    document.getElementById('alert').innerHTML = alertHTML;
-
-
-}// showAlert
+  document.getElementById("alert").innerHTML = alertHTML;
+} // showAlert
