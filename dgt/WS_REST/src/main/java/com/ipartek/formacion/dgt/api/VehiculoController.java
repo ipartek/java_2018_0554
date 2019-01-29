6dc63a0365bb018541e82e069e085feff876e4aa
AnderSolana <Curso@Formacion-TOSH6.ipartekaula.com>
2019-01-29 13:14:20 +0100

    Implementación de documentación de la API mediante Swagger: - (La dependencia estaba añadida en el archivo pom.xml). - Añadir la clase SwaggerConfig con la configuración básica de Swagger. - Añadir las pertinentes anotaciones para completar la documentación. [@ApiIgnore: No se añade la clase a la documentación]

--- dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/VehiculoController.java
+++ dgt/WS_REST/src/main/java/com/ipartek/formacion/dgt/api/VehiculoController.java
@@ -17,14 +17,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ipartek.appMultas.modelo.dao.CocheDAO;
 import com.ipartek.appMultas.modelo.pojo.Coche;
 import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @CrossOrigin
 @RestController
+@Api(tags = { "VEHICULO" }, produces="application/json", value="Gestión de Vehículos")
 //@RequestMapping({"/api/vehiculo/"}) Podemos poner uno general para todos los métodos que hay dentro
 public class VehiculoController {
 
@@ -46,6 +52,10 @@ public class VehiculoController {
 	 * @return OK(200) Si existen coches en la BD. Además, devolverá todos los coches.
 	 * @return NOT_FOUND(404) Si no existen coches en la BD.
 	 */
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message="Hay vehículos registrados en la Base de Datos, se devuelve una lista con ellos."),
+			@ApiResponse(code = 404, message="No existen coches en la Base de datos.")
+	})
 	@RequestMapping( value= {"/api/vehiculo"}, method = RequestMethod.GET)
 	public ResponseEntity<ArrayList<Coche>> listar(){
 		ResponseEntity<ArrayList<Coche>> response = new ResponseEntity<ArrayList<Coche>>(HttpStatus.NOT_FOUND);
@@ -63,6 +73,11 @@ public class VehiculoController {
 	 * @return OK(200) Si el coche existe. Además, se devolverán los datos del coche.
 	 * @return BAD_REQUEST(400) Si el formato del ID es incorrecto (letras).
 	 */
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message="El vehículo existe, se devolverán los datos del mismo."),
+			@ApiResponse(code = 400, message="El formato del ID no es válido (P.E: El ID no está en formato numérico)."),
+			@ApiResponse(code = 404, message="El ID del vehículo introducido no existe en la Base de Datos.")
+	})
 	@RequestMapping( value= {"/api/vehiculo/{id}"}, method = RequestMethod.GET)
 	public ResponseEntity<Coche> detalle(@PathVariable Long id) {
 		ResponseEntity<Coche> response = new ResponseEntity<Coche>(HttpStatus.NOT_FOUND);
@@ -85,6 +100,12 @@ public class VehiculoController {
 	 * @return CONFLICT (409) Si no puede eliminarse porque tiene multas asociadas.
 	 * @return OK (200) Si se ha eliminado correctamente.
 	 */
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message="El vehículo se ha eliminado correctamente."),
+			@ApiResponse(code = 400, message="El formato del ID no es válido (P.E: El ID no está en formato numérico)."),
+			@ApiResponse(code = 404, message="El ID del vehículo introducido no existe en la Base de Datos."),
+			@ApiResponse(code = 409, message="El vehículo no puede eliminarse porque tiene multas asociadas en la Base de Datos.")
+	})
 	@RequestMapping( value= {"/api/vehiculo/{id}"}, method = RequestMethod.DELETE)
 	public ResponseEntity<Coche> eliminar(@PathVariable Long id) {
 		ResponseEntity<Coche> response = new ResponseEntity<Coche>(HttpStatus.NOT_FOUND);
@@ -114,6 +135,11 @@ public class VehiculoController {
 	 * @return OK (200) El coche se ha dado de baja.
 	 * @return BAD_REQUEST (400) Si el formato del ID no es válido (letras).
 	 */
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message="El vehículo ha sido dado de baja."),
+			@ApiResponse(code = 400, message="El formato del ID no es válido (P.E: El ID no está en formato numérico)."),
+			@ApiResponse(code = 404, message="El ID del vehículo introducido no existe en la Base de Datos.")
+	})
 	@RequestMapping( value= {"/api/vehiculo/{id}"}, method = RequestMethod.PATCH)
 	public ResponseEntity<Coche> darDeBaja(@PathVariable Long id, @RequestBody Coche coche) {
 		ResponseEntity<Coche> response = new ResponseEntity<Coche>(HttpStatus.NOT_FOUND);
@@ -132,15 +158,20 @@ public class VehiculoController {
 	/**
 	 * Método que permite la creación de un nuevo coche en la base de datos.
 	 * @param coche a insertar en la base de datos.
-	 * @return BAD_REQUEST (400) Si los datos del coche no son correctos.  P.E: sin matrícula o vacía
-	 * @return NOT_FOUND (404) Si ocurre un error inesperado.
+	 * @return BAD_REQUEST (400) Si los datos del coche no son correctos.  P.E: sin matrícula o vacía.
 	 * @return CONFLICT (409) Si la matricula del coche a insertar ya existe en la BD.
 	 * @return CREATED (201) Si la inserción es correcta.
 	 * @return INTERNAL_SERVER_ERROR (500) Si ocurre cualquier otra Excepción (Mirar log para más info).
 	 */
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message="Se ha creado un nuevo vehículo"),
+			@ApiResponse(code = 409, message="La matrícula del vehículo a insertar ya existe"),
+			@ApiResponse(code = 400, message="Los datos del vehículo no son válidos (P.E: formato erróneo, fuera de la longitud establecida, etc.)")
+	})
+	@ResponseStatus(HttpStatus.CREATED)
 	@RequestMapping( value= {"/api/vehiculo/"}, method = RequestMethod.POST)
 	public ResponseEntity<Coche> crear(@RequestBody Coche coche) {
-		ResponseEntity<Coche> response = new ResponseEntity<Coche>(HttpStatus.NOT_FOUND);
+		ResponseEntity<Coche> response = new ResponseEntity<Coche>(HttpStatus.INTERNAL_SERVER_ERROR);
 			//TODO Crear coche
 			try {
 				Set<ConstraintViolation<Coche>> violations = validator.validate(coche);
@@ -174,6 +205,10 @@ public class VehiculoController {
 	 * @return NOT_FOUND(404) Ha ocurrido un error actualizando el coche, P.E: Datos no válidos (error en el formato).
 	 * @return OK(200) El coche ha sido actualizado correctamente.
 	 */
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message="El vehículo ha sido actualizado con éxito."),
+			@ApiResponse(code = 404, message="Ha ocurrido un error actualizando el coche, P.E: Datos no válidos (error en el formato).")
+	})
 	@RequestMapping( value= {"/api/vehiculo/{id}"}, method = RequestMethod.PUT)
 	public ResponseEntity<Coche> modificar(@PathVariable Long id, @RequestBody Coche coche) {
 		ResponseEntity<Coche> response = new ResponseEntity<Coche>(HttpStatus.NOT_FOUND);
