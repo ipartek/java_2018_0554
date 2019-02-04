ac2eb7ef5a58f591fbc40196df3fa88f346fd4e8
Curso <Curso@Formacion-TOS12.ipartekaula.com>
2019-02-04 12:20:31 +0100

    rest service persona funcionando

---
+++ taller/src/main/java/com/ipartek/formacion/taller/api/controller/PersonaController.java
@@ -0,0 +1,34 @@
+package com.ipartek.formacion.taller.api.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ipartek.formacion.taller.modelo.pojo.Persona;
+
+@CrossOrigin
+@RestController
+public class PersonaController {
+	
+
+	// LISTAR
+	@RequestMapping(value = { "/api/persona" }, method = RequestMethod.GET)
+	public ArrayList<Persona> listar() {
+
+		//TODO crear dao
+		ArrayList<Persona> personas = new ArrayList<Persona>(); 
+		Persona p = new Persona ();
+		p.setNombre("Manolo");
+		personas.add(p);
+		
+		p = new Persona ();
+		p.setNombre("Jose Mari");
+		personas.add(p);
+		
+		return personas;
+	}
+
+}
