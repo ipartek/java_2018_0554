4336ac5fdfbd7e1f921509d1d4d254d432d5e873
anderuraga <ander.uraga@gmail.com>
2019-02-04 13:30:41 +0100

    DAO terminado

--- taller/src/main/java/com/ipartek/formacion/taller/api/controller/PersonaController.java
+++ taller/src/main/java/com/ipartek/formacion/taller/api/controller/PersonaController.java
@@ -2,33 +2,29 @@ package com.ipartek.formacion.taller.api.controller;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ipartek.formacion.taller.modelo.pojo.Persona;
+import com.ipartek.formacion.taller.service.PersonaService;
 
 @CrossOrigin
 @RestController
 public class PersonaController {
 	
+	/* instancia e implementa patron singleton - inyeccion de dependencias */	
+	@Autowired
+	PersonaService personaService;
+	
 	
 	@RequestMapping( value= {"/api/persona"}, method = RequestMethod.GET)
 	public ArrayList<Persona> listar(){		
 		
-		//TODO crear DAO
-		ArrayList<Persona> personas = new ArrayList<Persona>();
-		
-		Persona p = new Persona();
-		p.setNombre("Manolo");
-		personas.add(p);
-		
-		p = new Persona();
-		p.setNombre("Jose MAri");
-		personas.add(p);		
-		
-		return personas;
+		//llamamos al servicio, NO al DAO		
+		return personaService.listar();
 	}
 
 }
---
+++ taller/src/main/java/com/ipartek/formacion/taller/service/PersonaService.java
@@ -0,0 +1,25 @@
+package com.ipartek.formacion.taller.service;
+
+import java.util.ArrayList;
+
+import com.ipartek.formacion.taller.modelo.pojo.Persona;
+
+
+
+public interface PersonaService {
+
+	/**
+	 * Listado Personas 
+	 * @return ArrayList<Persona>
+	 */
+	ArrayList<Persona> listar();
+	
+	/**
+	 * Lsitado Vehiculos de una Persona
+	 * @param idPersona identificador de la Persona
+	 * @return ArrayList<Vehiculo>
+	 */
+	ArrayList<Object> vehiculos( int idPersona );
+	
+	
+}
