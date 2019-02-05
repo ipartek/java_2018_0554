9ff361eb7bf708d0e84b2fc70db8c647d1f4265b
AnderSolana <Curso@Formacion-TOSH6.ipartekaula.com>
2019-02-05 14:10:50 +0100

    Validaciones implementadas para la creación y modificación de combustibles (javax validation)

--- taller/src/main/java/com/ipartek/formacion/taller/service/impl/CombustibleServiceImpl.java
+++ taller/src/main/java/com/ipartek/formacion/taller/service/impl/CombustibleServiceImpl.java
@@ -2,6 +2,12 @@ package com.ipartek.formacion.taller.service.impl;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +20,17 @@ import com.ipartek.formacion.taller.service.exception.CombustibleException;
 @Service
 public class CombustibleServiceImpl implements CombustibleService {
 
+	private ValidatorFactory factory;
+	private Validator validator;
+
 	@Autowired
 	CombustibleDAO combustibleDAO;
+
+	public CombustibleServiceImpl() {
+		super();
+		factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
+	}
 
 	@Override
 	public List<Combustible> listar() {
@@ -42,7 +57,12 @@ public class CombustibleServiceImpl implements CombustibleService {
 	public boolean crear(Combustible combustible) throws CombustibleException {
 		boolean isInserted = false;
 		try {
-			isInserted = combustibleDAO.insert(combustible);
+			Set<ConstraintViolation<Combustible>> violations = validator.validate(combustible);
+			if (violations.size() > 0) {
+				throw new CombustibleException(violations.toString());
+			} else {
+				isInserted = combustibleDAO.insert(combustible);
+			}
 		} catch (SQLException e) {
 			throw new CombustibleException(CombustibleException.EXCEPTION_EXIST);
 		}
@@ -53,7 +73,12 @@ public class CombustibleServiceImpl implements CombustibleService {
 	public boolean modificar(Combustible combustible) throws CombustibleException {
 		boolean isUpdated = false;
 		try {
-			isUpdated = combustibleDAO.update(combustible);
+			Set<ConstraintViolation<Combustible>> violations = validator.validate(combustible);
+			if (violations.size() > 0) {
+				throw new CombustibleException(violations.toString());
+			} else {
+				isUpdated = combustibleDAO.update(combustible);
+			}
 		} catch (SQLException e) {
 			throw new CombustibleException(CombustibleException.EXCEPTION_CONSTRAINT);
 		}
