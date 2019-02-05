5f2c62041c7163417a62740857579b44852a6dac
dZallo <danielzallo@gmail.com>
2019-02-05 13:30:42 +0100

    POJO para generar mensajes personalizados

---
+++ taller/src/main/java/com/ipartek/formacion/taller/api/pojo/Mensaje.java
@@ -0,0 +1,29 @@
+package com.ipartek.formacion.taller.api.pojo;
+
+public class Mensaje {
+	private String mensaje;
+
+	public Mensaje() {
+		super();
+		this.mensaje = "";
+	}
+
+	public Mensaje(String mensaje) {
+		this();
+		setMensaje(mensaje);
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+	@Override
+	public String toString() {
+		return "Mensaje [mensaje=" + mensaje + "]";
+	}
+
+}
