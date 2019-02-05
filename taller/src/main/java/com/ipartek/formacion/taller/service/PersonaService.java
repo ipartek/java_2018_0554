956ba5773f14eab1431fda0c2de6ea8262dbd3d7
anderuraga <ander.uraga@gmail.com>
2019-02-05 10:30:28 +0100

    implementar servicio

--- taller/src/main/java/com/ipartek/formacion/taller/service/PersonaService.java
+++ taller/src/main/java/com/ipartek/formacion/taller/service/PersonaService.java
@@ -3,6 +3,7 @@ package com.ipartek.formacion.taller.service;
 import java.util.ArrayList;
 
 import com.ipartek.formacion.taller.modelo.pojo.Persona;
+import com.ipartek.formacion.taller.modelo.pojo.Vehiculo;
 
 
 
@@ -15,11 +16,19 @@ public interface PersonaService {
 	ArrayList<Persona> listar();
 	
 	/**
-	 * Lsitado Vehiculos de una Persona
+	 * Listado Vehiculos de una Persona
 	 * @param idPersona identificador de la Persona
 	 * @return ArrayList<Vehiculo>
 	 */
-	ArrayList<Object> vehiculos( int idPersona );
+	ArrayList<Vehiculo> vehiculos( int idPersona );
 	
+	
+	/**
+	 * Detalle de un Vehiculo de una Persona
+	 * @param idPersona int identificador Persona
+	 * @param idVehiculo int identificador Vehiculo
+	 * @return vehiculo si lo encuentra, null si no existe
+	 */
+	Vehiculo vehiculo( int idPersona, int idVehiculo );
 	
 }
