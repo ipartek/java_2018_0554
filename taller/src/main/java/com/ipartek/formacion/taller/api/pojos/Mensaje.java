d62636c8bdd5cb9fe9958f5c3bf8bfa186687948
andonishiraiwa <Curso@Formacion-TOS13.ipartekaula.com>
2019-02-06 09:40:22 +0100

    integracion de mensajes personalizados para las validaciones

--- taller/src/main/java/com/ipartek/formacion/taller/api/pojos/Mensaje.java
+++ taller/src/main/java/com/ipartek/formacion/taller/api/pojos/Mensaje.java
@@ -1,12 +1,21 @@
 package com.ipartek.formacion.taller.api.pojos;
 
+import java.util.ArrayList;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+
+import com.ipartek.formacion.taller.modelo.pojo.Combustible;
+
 public class Mensaje {
 
 	private String mensaje;
+	private ArrayList<ErrorMensaje> errores;
 
 	public Mensaje() {
 		super();
 		this.mensaje = "";
+		this.errores = null;
 	}
 
 	public Mensaje(String mensaje) {
@@ -20,6 +29,70 @@ public class Mensaje {
 
 	public void setMensaje(String mensaje) {
 		this.mensaje = mensaje;
+	}
+
+	public ArrayList<ErrorMensaje> getErrores() {
+		return errores;
+	}
+
+	public void setErrores(ArrayList<ErrorMensaje> errores) {
+		this.errores = errores;
+	}
+
+	@Override
+	public String toString() {
+		return "Mensaje [mensaje=" + mensaje + ", errores=" + errores + "]";
+	}
+
+	// clase Interna 
+
+	class ErrorMensaje {
+
+		private String campo;
+		private String violacion;
+
+		ErrorMensaje() {
+			super();
+			this.campo = "";
+			this.violacion = "";
+		}
+
+		public ErrorMensaje(String campo, String violacion) {
+			this();
+			this.campo = campo;
+			this.violacion = violacion;
+		}
+
+		public String getCampo() {
+			return campo;
+		}
+
+		public void setCampo(String campo) {
+			this.campo = campo;
+		}
+
+//NUEVA PROPIEDAD, NUEVOS GET/SET
+		public String getViolacion() {
+			return violacion;
+		}
+
+		public void setViolacion(String violacion) {
+			this.violacion = violacion;
+		}
+
+	}
+
+	// metodo que asigna tipos de mensaje según los resultados de Violation
+
+	public void addViolations(Set<ConstraintViolation<Combustible>> violations) {
+
+		if (violations != null) {
+			this.errores = new ArrayList<ErrorMensaje>();
+			for (ConstraintViolation<Combustible> v : violations) {
+				ErrorMensaje errorMensaje = new ErrorMensaje(v.getPropertyPath().toString(), v.getMessage());
+				this.errores.add(errorMensaje);
+			}
+		}
 	}
 
 }
