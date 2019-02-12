bf7b6be13edf79538285878af3fe9e6ae0a83f38
AnderSolana <andersolanad@gmail.com>
2019-02-12 13:22:34 +0100

    Initial Import II

---
+++ angular/src/app/prueba/prueba.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prueba',
+  templateUrl: './prueba.component.html',
+  styleUrls: ['./prueba.component.scss']
+})
+export class PruebaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
