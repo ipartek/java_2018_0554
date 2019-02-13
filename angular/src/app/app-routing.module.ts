628fc31e3f03e6912af1b90e6a8fec4c4381c055
dZallo <danielzallo@gmail.com>
2019-02-13 11:00:31 +0100

    Navegador con rutas

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PruebaComponent } from './components/prueba/prueba.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'game',component:PruebaComponent},
+  {path:'about',component:AboutComponent},
+  {path:'**',pathMatch:'full',redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PruebaComponent } from './prueba/prueba.component';
-import { OtromasComponent } from './otromas/otromas.component';
+import { PruebaComponent } from './components/prueba/prueba.component';
+import { OtromasComponent } from './components/otromas/otromas.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PruebaComponent,
-    OtromasComponent
+    OtromasComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
