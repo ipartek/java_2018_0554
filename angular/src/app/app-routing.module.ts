62d54130e3048021474d67443047bb098f2337d5
anderuraga <ander.uraga@gmail.com>
2019-02-13 12:00:48 +0100

    navegacion terminada

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PruebaComponent } from './components/prueba/prueba.component';
+import { Error404Component } from './components/error404/error404.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },  
+  { path: 'juego', component: PruebaComponent },  
+  { path: '404', component: Error404Component }, 
+  { path: '**', pathMatch:'full', redirectTo : '404'  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PruebaComponent } from './prueba/prueba.component';
+
+// nuestros components
+import { PruebaComponent } from './components/prueba/prueba.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { Error404Component } from './components/error404/error404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PruebaComponent
+    PruebaComponent,
+    HomeComponent,
+    AboutComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
