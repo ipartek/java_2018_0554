ce15779b3b17fc12882207bd5d3cbe2bd07b469b
imanol <imahernando@gmail.com>
2019-02-13 09:33:49 +0100

    juego click con bootstrap

--- angular/src/app/prueba/prueba.component.ts
+++ angular/src/app/prueba/prueba.component.ts
@@ -1,30 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 
+
+const TIEMPO_JUEGO: number = 10;
+
 @Component({
   selector: 'app-prueba',
   templateUrl: './prueba.component.html',
   styleUrls: ['./prueba.component.scss']
 })
 export class PruebaComponent implements OnInit {
-  //variable tipo number
-  contador:number;
-  tiempo:number;
 
-  constructor() { 
+  // variable tipo numerica
+  contador: number;
+  tiempo: number;
+  intervalo;
+
+  constructor() {
     console.trace('PruebaComponent constructor');
-    //inicilizar las variables en el constructor
-    this.contador=0;
-    this.tiempo=10;
 
+    //inicializar las variables
+    this.contador = 0;
+    this.tiempo = TIEMPO_JUEGO; //seg
   }
 
   ngOnInit() {
     console.trace('PruebaComponent ngOnInit');
   }
 
+  restart(){
+    console.trace('click restart');
+    this.tiempo = TIEMPO_JUEGO;
+    this.contador = 0;
+  }
+
+  start(){
+    console.trace('click start');
+
+    let that = this;    
+    let interval = setInterval(function() {      
+      console.debug('restar');  
+      if ( that.tiempo > 0){   
+        that.tiempo--;       
+      }else{
+        console.debug('parar');  
+        this.clearInterval(interval);
+      }  
+    }, 1000);
+    
+  }
+
   sumar(){
-    console.trace('botonSumar++');
-    this.contador++;
+    console.trace('click botton sumar');
+    if ( this.tiempo > 0 ){
+      this.contador++;
+    }  
   }
 
 }
