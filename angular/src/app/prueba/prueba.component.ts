7ea9a1ef57bdd4aa78f1e7ea4b6f8038708fe919
anderuraga <ander.uraga@gmail.com>
2019-02-13 09:18:12 +0100

    juego click terminado

--- angular/src/app/prueba/prueba.component.ts
+++ angular/src/app/prueba/prueba.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+
+const TIEMPO_JUEGO: number = 10;
+
 @Component({
   selector: 'app-prueba',
   templateUrl: './prueba.component.html',
@@ -10,29 +13,39 @@ export class PruebaComponent implements OnInit {
   // variable tipo numerica
   contador: number;
   tiempo: number;
+  intervalo;
 
   constructor() {
     console.trace('PruebaComponent constructor');
 
     //inicializar las variables
     this.contador = 0;
-    this.tiempo = 10; //seg
+    this.tiempo = TIEMPO_JUEGO; //seg
   }
 
   ngOnInit() {
     console.trace('PruebaComponent ngOnInit');
   }
 
+  restart(){
+    console.trace('click restart');
+    this.tiempo = TIEMPO_JUEGO;
+    this.contador = 0;
+  }
+
   start(){
     console.trace('click start');
-    let that = this;
-    
-    setInterval(function() {      
-      console.debug('restar');
-      if( that.tiempo > 0 ){
-        that.tiempo = that.tiempo - 1;
+
+    let that = this;    
+    let interval = setInterval(function() {      
+      console.debug('restar');  
+      if ( that.tiempo > 0){   
+        that.tiempo--;       
+      }else{
+        console.debug('parar');  
+        this.clearInterval(interval);
       }  
-  }, 1000);
+    }, 1000);
     
   }
 
