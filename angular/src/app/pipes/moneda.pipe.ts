cb0ddf5af7204d925e420318dc3cba843e16ca26
AnderSolana <andersolanad@gmail.com>
2019-02-14 13:35:19 +0100

    Implementación de Pipe propio

--- angular/src/app/pipes/moneda.pipe.ts
+++ angular/src/app/pipes/moneda.pipe.ts
@@ -5,8 +5,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MonedaPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any { // ? Significa que el parámetro es opcional (Puede que venga o puede que no)
-    return null;
+  /**
+   * Transforma un número, añadiendo al final el símbolo de la moneda
+   * @param numero : number => Número a transformar
+   * @param locale : string => Por defecto 'es', cambiamos: 
+   *           <ol>
+   *              <li> es => '€' </li>
+   *              <li> en => '$' </li>
+   *           </ol>
+   */
+  transform(numero: number, locale: string = 'es'): string { // ? Significa que el parámetro es opcional (Puede que venga o puede que no)
+    let simbolo;
+    switch (locale) {
+      case 'es':
+        simbolo='€';
+        break;
+      case 'eu':
+      simbolo='$';
+        break;
+      default:
+      simbolo='€';
+        break;
+    }
+
+    return numero + ' ' + simbolo;
   }
 
 }
