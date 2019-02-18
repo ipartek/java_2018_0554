e639dca32b5d2176129f106f632078b79fda4762
imanol <imahernando@gmail.com>
2019-02-18 12:40:26 +0100

    fruta hardcodeado

--- angular/src/app/components/fruta/fruta.component.ts
+++ angular/src/app/components/fruta/fruta.component.ts
@@ -6,9 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fruta.component.scss']
 })
 export class FrutaComponent implements OnInit {
-
-  constructor() { }
-
+fruta:any;
+  constructor() {
+    this.fruta = 
+      {'nombre': 'pomelo',
+      'oferta': false,
+      'precio': 7.43,
+      'descuento': 7,
+      'imagen': 'http://frutasfercas.com/wp-content/uploads/2018/03/pomelo-1.jpg',
+      'colores': [
+        {'nombre': 'rojo', 'codigo': '#F00'},
+        {'nombre': 'naranja', 'codigo': '#FFA500'},
+      ]
+      };
+  }
+  calcularDescuento(fruta:any):number{
+    return (fruta.precio - (fruta.precio)*fruta.descuento/100);
+ }
   ngOnInit() {
   }
 
