3ca6695f0859e24e860d8d346042635eca0aaba7
AnderSolana <andersolanad@gmail.com>
2019-02-18 14:26:44 +0100

    Ejemplos con la clase Persona

--- angular/src/app/model/persona.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Persona } from './persona';
-
-describe('Persona', () => {
-  it('should create an instance', () => {
-    expect(new Persona()).toBeTruthy();
-  });
-});
--- angular/src/app/model/persona.ts
+++ angular/src/app/model/persona.ts
@@ -1,2 +1,26 @@
 export class Persona {
+
+    //Atributos privados y comienzan por guión bajo '_'
+    private _nombre: string;
+
+    // Solo se puede declarar un constructor
+    constructor( nombre?: string){
+        this._nombre = (nombre) ? nombre : 'Anonimo';
+        
+        // if(nombre){
+        //     this._nombre=nombre;
+        // }else{
+        //     this._nombre='Anónimo';
+        // }
+    }
+
+    //Getters y Setters
+    public get nombre(): string {
+        console.trace('getter ' + this._nombre);
+        return this._nombre;
+    }
+    public set nombre(value: string) {
+        console.trace('setter ' + this._nombre);
+        this._nombre = value;
+    }
 }
