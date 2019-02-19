f496ce333fefc2be45e5783cc50119724f809724
amaia9119 <delolmoamaia@gmail.com>
2019-02-19 09:57:51 +0100

    ultimos cambios home

--- angular/src/app/components/home/home.component.ts
+++ angular/src/app/components/home/home.component.ts
@@ -8,15 +8,46 @@ import { Persona } from 'src/app/model/persona';
 })
 export class HomeComponent implements OnInit {
 
-  manolo: Persona; 
-  constructor() { 
-    this.manolo;
+  manolo: Persona;
+  sinNombre: Persona;
+
+  idiomas: string[];
+  idiomaSeleccionado: string;
+  textoTraducir: string;
+
+
+  constructor() {
+    
+
+    this.idiomas = ['eu','es','en'];
+    this.idiomaSeleccionado = this.idiomas[0]; //eu
+    this.textoTraducir = '';
+
+    this.manolo = new Persona('Manolo');
+    this.sinNombre = new Persona();
+ 
+
     console.debug("manolo sin inicializar %o", this.manolo);
+
     this.manolo.nombre = 'Manolo';
-    console.debug("Dame tu nombre " + this.manolo.nombre);
+  
+
+    console.debug("Dame tu nombre " + this.manolo.nombre );
+
+
   }
 
   ngOnInit() {
+  }
+
+
+  cambiarIdioma( idioma: string){
+    this.idiomaSeleccionado = idioma;
+  }
+
+  escucho(event){
+
+    alert('Acabo recicbir evento del Hijo idioma=' + event.idioma + ' y texto=' + event.texto);
   }
 
 }
--- angular/src/app/components/traductor/traductor.component.ts
+++ angular/src/app/components/traductor/traductor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-traductor',
@@ -6,16 +6,36 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./traductor.component.scss']
 })
 export class TraductorComponent implements OnInit {
-  
+
   @Input('idioma') idioma: string;
+
   @Input('texto') texto: string;
 
+  // evento de salida, este nombre 'llamarPadre' se debe capturar en componentePadre
+  @Output() llamarPadre = new EventEmitter();
+  
   constructor() { 
     console.trace('TraductorComponent constructor');
   }
 
   ngOnInit() {
     console.trace('TraductorComponent ngOnInit');
+  }
+
+  /**
+   * Al realizar click sobre el boton, tenemos que emitir un evento para avisar al padre
+   * usando la nombre del @Output llamarPadre: EventEmitter
+   * @param event 
+   */
+  clickBotton(event){
+
+    let datosEnviar = {
+        'idioma': this.idioma,
+        'texto': this.texto
+    };
+
+    // llamarPadre => @Output hay que usar la funcion "EMIT"
+    this.llamarPadre.emit( datosEnviar );
   }
 
 }
