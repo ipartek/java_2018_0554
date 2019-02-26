d8a1c22f5241406da8cd49fa5a3e8e9403f41fb5
AnderSolana <andersolanad@gmail.com>
2019-02-26 13:09:08 +0100

    Creados los atributos de la clase Multa, su constructor y sus Getters y Setters

--- dgt/angular/src/app/model/multa.ts
+++ dgt/angular/src/app/model/multa.ts
@@ -1,2 +1,44 @@
 export class Multa {
+
+    private _id: number;
+    private _importe: number;
+    private _concepto: string;
+    private _fecha: string;
+
+    constructor(id: number, importe: number, concepto: string, fecha: string) {
+        this._id = id;
+        this._importe = importe;
+        this._concepto = concepto;
+        this._fecha = fecha;
+    }
+
+
+    //Getters y Setters
+    public get id(): number {
+        return this._id;
+    }
+    public set id(value: number) {
+        this._id = value;
+    }
+    public get importe(): number {
+        return this._importe;
+    }
+    public set importe(value: number) {
+        this._importe = value;
+    }
+    public get concepto(): string {
+        return this._concepto;
+    }
+    public set concepto(value: string) {
+        this._concepto = value;
+    }
+    public get fecha(): string {
+        return this._fecha;
+    }
+    public set fecha(value: string) {
+        this._fecha = value;
+    }
+
+
+
 }
