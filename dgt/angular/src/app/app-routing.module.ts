d8cd36bfb69b76e01a8aaf067663725c7ad989e6
AnderSolana <andersolanad@gmail.com>
2019-02-26 11:50:11 +0100

    Nuevo componente 'home'. Cambios en el routing de la aplicación, configurando su navegabilidad

--- dgt/angular/src/app/app-routing.module.ts
+++ dgt/angular/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    data: { title: 'login' }
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Home' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- dgt/angular/src/app/app.module.ts
+++ dgt/angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
