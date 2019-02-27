38b0d9bce4af8aaf7089cf4c580c5d928d603c71
AnderSolana <andersolanad@gmail.com>
2019-02-27 12:45:04 +0100

    Métodos a implementar del AgenteService añadidos

--- dgt/modelo/src/main/java/com/ipartek/appMultas/modelo/service/AgenteService.java
+++ dgt/modelo/src/main/java/com/ipartek/appMultas/modelo/service/AgenteService.java
@@ -1,6 +1,11 @@
 package com.ipartek.appMultas.modelo.service;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 import com.ipartek.appMultas.modelo.pojo.Agente;
+import com.ipartek.appMultas.modelo.pojo.Coche;
+import com.ipartek.appMultas.modelo.pojo.Multa;
 
 public interface AgenteService {
 	
@@ -11,6 +16,23 @@ public interface AgenteService {
 	 * @return El objeto agente registrado en la BD. Retorna un agente nulo si los datos introducidos no son correctos.
 	 */
 	Agente login(String placa, String password);
+	
+	/**
+	 * 
+	 * @param m Multa(Long id, Double importe, String concepto, Timestamp fecha, Coche coche)
+	 * @param idAgente Identificador del Agente
+	 * @return Multa 
+	 * @throws Exception Si el concepto es null, el idAgente o el idCoche no existen,
+	 * cuando importe < 0
+	 */
+	Multa multar(Multa m, Long idAgente) throws Exception;
+	
+	/**
+	 * Todas las multas de un agente
+	 * @param idAgente
+	 * @return listado, si no hay ninguna vacio, no null
+	 */
+	List<Multa> obtenerMultas(Long idAgente);
 	
 
 }
--- dgt/modelo/src/main/java/com/ipartek/appMultas/modelo/service/impl/AgenteServiceImpl.java
+++ dgt/modelo/src/main/java/com/ipartek/appMultas/modelo/service/impl/AgenteServiceImpl.java
@@ -1,9 +1,11 @@
 package com.ipartek.appMultas.modelo.service.impl;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.ipartek.appMultas.modelo.dao.AgenteDAO;
 import com.ipartek.appMultas.modelo.pojo.Agente;
+import com.ipartek.appMultas.modelo.pojo.Multa;
 import com.ipartek.appMultas.modelo.pojo.Objetivo;
 import com.ipartek.appMultas.modelo.service.AgenteService;
 
@@ -34,6 +36,18 @@ public class AgenteServiceImpl implements AgenteService{
 		//- Objetivo Mensual
 		//- Objetivo Meses ( ArrayList<Objetivo> )
 		return agenteDAO.login(placa, password);
+	}
+
+	@Override
+	public Multa multar(Multa m, Long idAgente) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Multa> obtenerMultas(Long idAgente) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
