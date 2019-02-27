c208ac6e9e26cdce827355aefe1393615eaf8dfe
AnderSolana <andersolanad@gmail.com>
2019-02-27 13:08:34 +0100

    Implementados los métodos (hardcodeados) para comprobar el login con numero de placa y pass 'admin'

--- dgt/angular/src/app/guards/permisos.guard.ts
+++ dgt/angular/src/app/guards/permisos.guard.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AutorizacionService } from '../providers/autorizacion.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PermisosGuard implements CanActivate {
+
+  constructor(
+    private autorizacionService: AutorizacionService,
+    private router: Router
+  ){
+    console.trace('PermisosGuard constructor');
+  }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    
+      console.trace('PermisosGuard canActivate');
+      if(this.autorizacionService.estaLogueado()){
+        console.debug('Login correcto');
+        return true;
+      }else{
+        console.warn('Login incorrecto');
+        this.router.navigate(['/login']);
+        return false;
+      }
+      
   }
   
 }
--- dgt/angular/src/app/providers/autorizacion.service.ts
+++ dgt/angular/src/app/providers/autorizacion.service.ts
@@ -5,5 +5,40 @@ import { Injectable } from '@angular/core';
 })
 export class AutorizacionService {
 
-  constructor() { }
+  private isLogged: boolean;
+
+  constructor() {
+    console.trace('AutorizacionService constructor');
+    this.isLogged = false;
+  } // Constructor
+
+  /**
+   * Nos indica si el usuario ha hecho login o no
+   * @returns True si ha hecho login, False si no
+   */
+  estaLogueado() {
+    console.trace('AutorizacionService estaLogueado()');
+    return this.isLogged;
+  } //estaLogueado
+
+  /**
+   * Llama a la API REST y comprueba que el usuario y la contraseña existan en la DB
+   * @param placa Nº de placa del Agente
+   * @param pass Contraseña del Agente
+   */
+  login(placa: string, pass: string): any {
+    if (placa === 'admin' && pass === 'admin') {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+  } // Login
+
+  /**
+  * Cierra la session del usuario llamando al backoffice
+  */
+  logout() {
+    //TODO llamar Servicio Rest
+    this.isLogged = false;
+  }
 }
