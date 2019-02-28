4ec51cbe2c4c6d3aeb55697e28476ca53f1812cc
AnderSolana <andersolanad@gmail.com>
2019-02-28 14:10:26 +0100

    Añadido el Interfaz que Ander va a usar con su test

--- dgt/modelo/src/main/java/com/ipartek/appMultas/modelo/service/AgenteService.java
+++ dgt/modelo/src/main/java/com/ipartek/appMultas/modelo/service/AgenteService.java
@@ -1,38 +1,40 @@
 package com.ipartek.appMultas.modelo.service;
 
-import java.sql.Timestamp;
 import java.util.List;
 
 import com.ipartek.appMultas.modelo.pojo.Agente;
-import com.ipartek.appMultas.modelo.pojo.Coche;
 import com.ipartek.appMultas.modelo.pojo.Multa;
 
 public interface AgenteService {
 	
+
 	/**
-	 * Comprueba que la placa y la contraseña recibidos por parámetros existen en la BD.
-	 * @param placa Nº de placa del agente.
-	 * @param password Contraseña del agente.
-	 * @return El objeto agente registrado en la BD. Retorna un agente nulo si los datos introducidos no son correctos.
+	 * 	comprueba que exista el Agente en la bbdd
+	 * @param numeroPlaca
+	 * @param password
+	 * @return Agente si existe, null si no existe
 	 */
-	Agente login(String placa, String password);
+	Agente existe( String numeroPlaca, String password);
+	
+
+	/**
+	 * Multar un Vehiculo
+	 * @param idCoche
+	 * @param idAgente
+	 * @param concepto
+	 * @param importe
+	 * @return
+	 * @throws Exception si el concepto es null, el idAgente o idCoche no existen, importe < 0
+	 */
+	Multa multar( int idCoche, int idAgente, String concepto, float importe  ) throws Exception;
+	
 	
 	/**
-	 * 
-	 * @param m Multa(Long id, Double importe, String concepto, Timestamp fecha, Coche coche)
-	 * @param idAgente Identificador del Agente
-	 * @return Multa 
-	 * @throws Exception Si el concepto es null, el idAgente o el idCoche no existen,
-	 * cuando importe < 0
-	 */
-	Multa multar(Multa m, Long idAgente) throws Exception;
-	
-	/**
-	 * Todas las multas de un agente
+	 * Todas las multas de un Agente
 	 * @param idAgente
 	 * @return listado, si no hay ninguna vacio, no null
 	 */
-	List<Multa> obtenerMultas(Long idAgente);
+	List<Multa> obtenerMultas(int idAgente);
 	
 
 }
