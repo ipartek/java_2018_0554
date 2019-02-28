2a0fda4593dbe3a98af5caa9de167dec64d28e15
AnderSolana <andersolanad@gmail.com>
2019-02-28 09:23:08 +0100

    Creadas las constantes de la clase alerta con los tipos de mensaje. Creados los atributos privados de la clase. Creado el constructor. Creados Getters y Setters

--- dgt/angular/src/app/model/alerta.ts
+++ dgt/angular/src/app/model/alerta.ts
@@ -1,2 +1,29 @@
 export class Alerta {
+
+    public static TIPO_ERROR = 'danger';
+    public static TIPO_SUCCESS = 'success';
+    public static TIPO_INFO = 'info';
+    public static TIPO_WARNING = 'warning';
+
+    private _texto: string;
+    private _tipo: string;
+
+    constructor(texto: string, tipo = Alerta.TIPO_ERROR){
+        this._texto = texto;
+        this._tipo = tipo;
+    }
+
+    public get texto(): string {
+        return this._texto;
+    }
+    public set texto(value: string) {
+        this._texto = value;
+    }
+    public get tipo(): string {
+        return this._tipo;
+    }
+    public set tipo(value: string) {
+        this._tipo = value;
+    }
+
 }
