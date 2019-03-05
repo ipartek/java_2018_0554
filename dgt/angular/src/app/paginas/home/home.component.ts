6e6676d0fd41c3296f9e8d4374aa575c21e5084f
AnderSolana <andersolanad@gmail.com>
2019-03-05 10:22:28 +0100

    Cambios para subida a producción

--- dgt/angular/src/app/paginas/home/home.component.ts
+++ dgt/angular/src/app/paginas/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  //
+  texto: string = environment.texto;
 
   constructor() { }
 
--- dgt/angular/src/environments/environment.prod.ts
+++ dgt/angular/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  texto: "PRODUCCIÓN",
+  base: '/angularAnder/'
 };
